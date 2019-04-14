99f1640dfe7f930c0e5ec1b420d2ae124148d3db
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2019-04-14 18:36:25 -0300

    Update various Apache Commons packages and its usages

--- src/main/java/biblivre/core/configurations/Configurations.java
+++ src/main/java/biblivre/core/configurations/Configurations.java
@@ -24,8 +24,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.log4j.Logger;
 
 import biblivre.core.StaticBO;
