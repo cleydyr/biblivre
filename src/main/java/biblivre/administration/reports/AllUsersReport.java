8451b6b3137845fed8f74499b3de9dab61a8bc8e
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2019-06-23 13:50:16 -0300

    [78] refactor

--- src/main/java/biblivre/administration/reports/AllUsersReport.java
+++ src/main/java/biblivre/administration/reports/AllUsersReport.java
@@ -23,7 +23,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 
+import com.lowagie.text.Chunk;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
@@ -65,9 +67,9 @@ public class AllUsersReport extends BaseBiblivreReport<AllUsersReportDto> {
 	}
 
 	private void _insertUserListByTypeHeader(Document document) throws DocumentException {
-		Paragraph p3 = new Paragraph(ReportUtil.getHeaderChunk(this.getText("administration.reports.field.user_list_by_type")));
-		p3.setAlignment(Element.ALIGN_LEFT);
-		document.add(p3);
+		insertChunkText(
+				document, ReportUtil::getHeaderChunk, Element.ALIGN_LEFT,
+				this.getText("administration.reports.field.user_list_by_type"));
 		ReportUtil.insertNewLine(document);
 	}
 
@@ -78,10 +80,16 @@ public class AllUsersReport extends BaseBiblivreReport<AllUsersReportDto> {
 	}
 
 	private void _insertUserCountByTypeHeader(Document document) throws DocumentException {
-		Paragraph p2 = new Paragraph(ReportUtil.getHeaderChunk(this.getText("administration.reports.field.user_count_by_type")));
-		p2.setAlignment(Element.ALIGN_LEFT);
-		document.add(p2);
+		insertChunkText(
+				document, ReportUtil::getHeaderChunk, Element.ALIGN_LEFT,
+				this.getText("administration.reports.field.user_count_by_type"));
 		ReportUtil.insertNewLine(document);
+	}
+
+	public void insertChunkText(Document document, Function<String, Chunk> chunker, int alignment, String text) throws DocumentException {
+		Paragraph p2 = new Paragraph(chunker.apply(text));
+		p2.setAlignment(alignment);
+		document.add(p2);
 	}
 
 	private void _insertReportTitle(Document document) throws DocumentException {
