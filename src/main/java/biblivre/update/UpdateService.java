f919cfee03e1178618a6163c592304c82078d952
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2019-08-18 13:35:00 -0300

    [127] remova método não utilizado e introduza outro para processar comandos após a atualização do banco de dados

--- src/main/java/biblivre/update/UpdateService.java
+++ src/main/java/biblivre/update/UpdateService.java
@@ -3,16 +3,14 @@ package biblivre.update;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import biblivre.core.UpdatesDAO;
-
 public interface UpdateService {
 	public String getVersion();
 
-	public default void end(Connection connection, UpdatesDAO dao) throws SQLException {
-		dao.commitUpdate(getVersion(), connection);
+	public default void doUpdate(Connection connection) throws SQLException {
+		// Do nothing
 	}
 
-	public default void doUpdate(Connection connection) throws SQLException {
+	public default void afterUpdate() {
 		// Do nothing
 	}
 }
