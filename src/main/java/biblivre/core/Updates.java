f6e78a289d39a36f59eab382c65094a8cf01bc4c
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2019-08-18 13:02:07 -0300

    [127] novo update

--- src/main/java/biblivre/core/Updates.java
+++ src/main/java/biblivre/core/Updates.java
@@ -66,12 +66,6 @@ public class Updates {
 
 			String version = null;
 
-			version = "4.1.11";
-			if (!installedVersions.contains(version)) {
-				con = dao.beginUpdate();
-				dao.commitUpdate(version, con);
-			}
-
 			version = "5.0.0";
 			if (!installedVersions.contains(version)) {
 				con = dao.beginUpdate();
---
+++ src/main/java/biblivre/update/v4_1_11/Update.java
@@ -0,0 +1,12 @@
+package biblivre.update.v4_1_11;
+
+import biblivre.update.UpdateService;
+
+public class Update implements UpdateService {
+
+	@Override
+	public String getVersion() {
+		return "4.1.11";
+	}
+
+}
