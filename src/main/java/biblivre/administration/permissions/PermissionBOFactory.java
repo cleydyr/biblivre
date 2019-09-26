4bc57f72cc6601633bdc0872db5f5503e4444f8f
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2019-09-26 20:05:21 -0300

    remove method, will put later, when needed

--- src/main/java/biblivre/administration/permissions/PermissionBOFactory.java
+++ src/main/java/biblivre/administration/permissions/PermissionBOFactory.java
@@ -1,7 +1,5 @@
 package biblivre.administration.permissions;
 
 public interface PermissionBOFactory {
-	public PermissionBO getInstance(String datasource, String schema);
-
 	public PermissionBO getInstance(String schema);
 }
