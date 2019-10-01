d8e5d29a732c1407a3d27a00da15ac4683bae65b
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2019-10-01 11:43:08 -0300

    aplique novo logger e refatore classe

--- src/main/java/biblivre/administration/permissions/Handler.java
+++ src/main/java/biblivre/administration/permissions/Handler.java
@@ -24,6 +24,8 @@ import java.util.Collection;
 
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
--- src/main/java/biblivre/core/controllers/Controller.java
+++ src/main/java/biblivre/core/controllers/Controller.java
@@ -26,7 +26,8 @@ import java.lang.reflect.Method;
 import javax.servlet.ServletException;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import biblivre.administration.permissions.PermissionConfiguration;
@@ -45,8 +46,9 @@ import biblivre.core.utils.TextUtils;
 
 
 public abstract class Controller {
-	
-	protected final Logger log = Logger.getLogger(this.getClass());
+
+	private static Logger _logger = LoggerFactory.getLogger(Controller.class);
+
 	protected ExtendedRequest xRequest;
 	protected ExtendedResponse xResponse;
 	protected AbstractHandler handler;
@@ -58,137 +60,208 @@ public abstract class Controller {
 		this.xResponse = xResponse;
 		this.headerOnly = false;
 	}
-	
-	protected void processRequest() throws ServletException, IOException {
-		String schema = null;
-		String module = null;
-		String action = null;
-		
-		this.xRequest.setCharacterEncoding(Constants.DEFAULT_CHARSET.name());
-		this.xResponse.setCharacterEncoding(Constants.DEFAULT_CHARSET.name());
-		
-		try {
-			schema = this.xRequest.getSchema();
-			module = this.xRequest.getString("module", (String) this.xRequest.getAttribute("module"));
-			action = this.xRequest.getString("action", (String) this.xRequest.getAttribute("action"));
-			
-			// In case of invalid pack and method, send user to index page
-			if (StringUtils.isBlank(module) || StringUtils.isBlank(action)) {
-				this.doError("error.void");
-				return;
-			}
 
-			if (State.LOCKED.get() && !action.equals("progress")) {
-				this.doLockedStateError();
-				return;	
-			}
-
-			boolean isSetup = (module.equals("administration.setup") || (module.equals("menu") && action.equals("setup")));
-			
-			if (isSetup && (Configurations.getBoolean(schema, Constants.CONFIG_NEW_LIBRARY) || action.equals("progress"))) {
-				// authorize
-			} else {	
-				AuthorizationPoints authPoints = (AuthorizationPoints) this.xRequest.getSessionAttribute(schema, "logged_user_atps");
-				if (authPoints == null) {
-					authPoints = AuthorizationPoints.getNotLoggedInstance(schema);
-				}
-	
-				AuthorizationBO abo = AuthorizationBO.getInstance(schema);
-				abo.authorize(authPoints, module, action);
-			}			
-		} catch (AuthorizationException e) {
-			// Exception thrown in abo.authorize
-			this.doAuthorizationError();
-			return;	
-		}
-		
-		try {
-			this.handlerClass = Class.forName("biblivre." + module + ".Handler");
-
-			try (AnnotationConfigApplicationContext context =
-					new AnnotationConfigApplicationContext(PermissionConfiguration.class)) {
-
-				this.handler = (AbstractHandler) context.getBean(this.handlerClass);
-			} catch(Exception e) {
-				System.out.println("handler not found?");
-			}
-
-			if (this.handler == null) {
-				this.handler = (AbstractHandler) this.handlerClass.newInstance();
-			}
-			
-			Class<?> validatorClass = Class.forName("biblivre." + module + ".Validator");
-			String validationMethodName = "validate_" + action;
-			Method validationMethod = validatorClass.getDeclaredMethod(TextUtils.camelCase(validationMethodName), AbstractHandler.class, ExtendedRequest.class, ExtendedResponse.class);
-			
-			AbstractValidator validator = (AbstractValidator) validatorClass.newInstance();
-			validationMethod.invoke(validator, this.handler, this.xRequest, this.xResponse);
-			if (!validator.checkValidation(this.handler)) {
-				this.doReturn();
-				return;
-			}
-		} catch (AuthorizationException e) {
-			this.doAuthorizationError();
-		} catch (ClassNotFoundException cnfe) {
-			//No Validator found, do nothing.
-		} catch (NoSuchMethodException nsme) {
-			//No Method found in the Validator, so do nothing.
-		} catch (InvocationTargetException e) {
-			// Exception thrown in method.invoke
-			Throwable handlerException = e.getTargetException();
-			if (handlerException instanceof AuthorizationException) {
-				this.doAuthorizationError();
-			} else {
-				this.doError("error.runtime_error", handlerException);
-			}
-			this.log.error(e.getMessage(), e);
-			return;
-		} catch (Exception e) {
-			// ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, etc.
-			this.doError("error.invalid_handler", e);
-			return;
-		}
-		
-		try {
-			Method method;
-			
-			try {
-				method = this.handlerClass.getDeclaredMethod(TextUtils.camelCase(action), ExtendedRequest.class, ExtendedResponse.class);
-			} catch (NoSuchMethodException e) {
-				method = this.handlerClass.getSuperclass().getDeclaredMethod(TextUtils.camelCase(action), ExtendedRequest.class, ExtendedResponse.class);
-			}
-
-			method.invoke(this.handler, this.xRequest, this.xResponse);
-		} catch (InvocationTargetException e) {
-			// Exception thrown in method.invoke
-			Throwable handlerException = e.getTargetException();
-
-			if (handlerException instanceof AuthorizationException) {
-				this.doAuthorizationError();
-			} else 	if (handlerException instanceof ValidationException) {
-				this.doWarning(handlerException.getMessage(), handlerException);
-			} else {
-				this.doError("error.runtime_error", handlerException);
-			}
-			this.log.error(e.getMessage(), e);
-			return;
-			
-		} catch (Exception e) {
-			// ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, etc.
-			this.doError("error.invalid_handler", e);
-			this.log.error(e.getMessage(), e);
-			return;
-		}
-
-		this.doReturn();
-	}
-	
 	public boolean isHeaderOnly() {
 		return this.headerOnly;
 	}
 
 	public void setHeaderOnly(boolean headerOnly) {
 		this.headerOnly = headerOnly;
+	}
+
+	protected void processRequest() throws ServletException, IOException {
+		
+		this.xRequest.setCharacterEncoding(Constants.DEFAULT_CHARSET.name());
+		this.xResponse.setCharacterEncoding(Constants.DEFAULT_CHARSET.name());
+		
+		String schema = this.xRequest.getSchema();
+		String module = this.xRequest.getString("module", (String) this.xRequest.getAttribute("module"));
+		String action = this.xRequest.getString("action", (String) this.xRequest.getAttribute("action"));
+
+		try {
+			// In case of invalid package and method, send user to index page
+			if (_checkValidRequestParameters(module, action)) {
+				this.doError("error.void");
+				return;
+			}
+
+			if (_checkLockedAndNotRequestingProgressInformation(action)) {
+				this.doLockedStateError();
+				return;	
+			}
+
+			if (!_isFirstSetup(schema, module, action)) {
+				_checkAuthorization(schema, module, action);
+			}			
+		} catch (AuthorizationException e) {
+			this.doAuthorizationError();
+
+			return;	
+		}
+		
+		try {
+			this.handlerClass = Class.forName("biblivre." + module + ".Handler");
+
+			this.handler = _tryGettingHandlerFromContextBean(this.handlerClass);
+
+			if (this.handler == null) {
+				this.handler = (AbstractHandler) this.handlerClass.newInstance();
+			}
+
+			if (!_isValidActionForModule(module, action)) {
+				this.doReturn();
+
+				return;
+			}
+		} catch (AuthorizationException e) {
+			this.doAuthorizationError();
+		} catch (ClassNotFoundException | NoSuchMethodException e) {
+			//No Validator found, do nothing.
+			//No Method found in the Validator, so do nothing.
+		} catch (InvocationTargetException e) {
+			// Exception thrown in method.invoke
+			Throwable handlerException = e.getTargetException();
+
+			if (handlerException instanceof AuthorizationException) {
+				this.doAuthorizationError();
+			} else {
+				this.doError("error.runtime_error", handlerException);
+			}
+
+			_logger.error(e.getMessage(), e);
+
+			return;
+		} catch (Exception e) {
+			// ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, etc.
+
+			_logger.warn("Unknown handler", e);
+
+			this.doError("error.invalid_handler", e);
+
+			return;
+		}
+		
+		try {
+			_executeAppropriatedMethodForAction(action);
+		} catch (InvocationTargetException e) {
+			_handleActionExecutionException(e);
+
+			return;
+		} catch (Exception e) {
+			_handleGenericException(e);
+
+			return;
+		}
+
+		this.doReturn();
+	}
+
+	private void _executeAppropriatedMethodForAction(String action)
+			throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+		Method method = null;
+
+		try {
+			method =
+					this.handlerClass.getDeclaredMethod(
+							TextUtils.camelCase(action), ExtendedRequest.class,
+							ExtendedResponse.class);
+		} catch (NoSuchMethodException e) {
+			Class<?> superclass = this.handlerClass.getSuperclass();
+
+			method =
+					superclass.getDeclaredMethod(
+							TextUtils.camelCase(action), ExtendedRequest.class,
+							ExtendedResponse.class);
+		}
+
+		method.invoke(this.handler, this.xRequest, this.xResponse);
+	}
+
+	private boolean _isValidActionForModule(String module, String action) throws Exception {
+		AbstractValidator validator = _getValidator(module, action);
+
+		return validator.checkValidation(this.handler);
+	}
+
+	private AbstractValidator _getValidator(String module, String action) throws Exception {
+		Class<?> validatorClass = Class.forName("biblivre." + module + ".Validator");
+
+		String validationMethodName = "validate_" + action;
+
+		Method validationMethod =
+				validatorClass.getDeclaredMethod(
+						TextUtils.camelCase(validationMethodName), AbstractHandler.class,
+						ExtendedRequest.class, ExtendedResponse.class);
+
+		AbstractValidator validator = (AbstractValidator) validatorClass.newInstance();
+
+		validationMethod.invoke(validator, this.handler, this.xRequest, this.xResponse);
+
+		return validator;
+	}
+
+	private void _checkAuthorization(String schema, String module, String action) {
+		AuthorizationPoints authPoints =
+				(AuthorizationPoints) this.xRequest.getSessionAttribute(schema,
+						"logged_user_atps");
+
+		if (authPoints == null) {
+			authPoints = AuthorizationPoints.getNotLoggedInstance(schema);
+		}
+
+		AuthorizationBO abo = AuthorizationBO.getInstance(schema);
+
+		abo.authorize(authPoints, module, action);
+	}
+
+	private boolean _isFirstSetup(String schema, String module, String action) {
+		return (module.equals("administration.setup") || (module.equals("menu") &&
+				action.equals("setup"))) &&
+				(Configurations.getBoolean(schema, Constants.CONFIG_NEW_LIBRARY) ||
+						action.equals("progress"));
+	}
+
+	private boolean _checkLockedAndNotRequestingProgressInformation(String action) {
+		return State.LOCKED.get() && !action.equals("progress");
+	}
+
+	private boolean _checkValidRequestParameters(String module, String action) {
+		return StringUtils.isBlank(module) || StringUtils.isBlank(action);
+	}
+
+	private AbstractHandler _tryGettingHandlerFromContextBean(Class<?> clazz) {
+		try (AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(PermissionConfiguration.class)) {
+
+			return (AbstractHandler) context.getBean(clazz);
+		} catch(Exception e) {
+			_logger.warn("Handler bean not found: ", e.getMessage());
+		}
+
+		return null;
+	}
+
+	private void _handleGenericException(Exception e) throws ServletException, IOException {
+		// ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, etc.
+		this.doError("error.invalid_handler", e);
+
+		_logger.error(e.getMessage(), e);
+	}
+
+	private void _handleActionExecutionException(InvocationTargetException e)
+			throws ServletException, IOException {
+		// Exception thrown in method.invoke
+		Throwable handlerException = e.getTargetException();
+
+		if (handlerException instanceof AuthorizationException) {
+			this.doAuthorizationError();
+		} else 	if (handlerException instanceof ValidationException) {
+			this.doWarning(handlerException.getMessage(), handlerException);
+		} else {
+			this.doError("error.runtime_error", handlerException);
+		}
+
+		_logger.error(e.getMessage(), e);
 	}
 
 	protected abstract void doReturn() throws ServletException, IOException;
