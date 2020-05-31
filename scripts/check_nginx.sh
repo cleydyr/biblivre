0a5226f1b34b1d00a0f8669f66b07937fd429da9
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2020-05-31 14:58:22 -0300

    #222 remake the validation with a real server running

---
+++ scripts/check_nginx.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+docker network create tmp >> /dev/null
+
+docker run --network tmp -t --rm -d --name app tomcat:9-jdk8 >> /dev/null
+docker run --network tmp --rm -t -a stdout --name tmp-nginx -v $PWD/nginx/conf.d/:/etc/nginx/conf.d:ro nginx nginx -c /etc/nginx/nginx.conf -t
+
+exitCode=$?
+
+if [ $exitCode -ne 0 ]; then
+    echo FAILED
+else
+    echo SUCCESS
+fi
+
+docker stop app >> /dev/null
+docker network rm tmp  >> /dev/null
+
+exit $exitCode
