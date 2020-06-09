16379409284d22e96487a5d0486f34c0e0f2534e
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2020-06-09 20:56:15 -0300

    refactor and avoid using connection directly

--- src/main/java/biblivre/acquisition/request/RequestDAO.java
+++ src/main/java/biblivre/acquisition/request/RequestDAO.java
@@ -19,8 +19,6 @@
  ******************************************************************************/
 package biblivre.acquisition.request;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.List;
 
@@ -29,261 +27,120 @@ import org.apache.commons.lang3.StringUtils;
 import biblivre.core.AbstractDAO;
 import biblivre.core.AbstractDTO;
 import biblivre.core.DTOCollection;
-import biblivre.core.PagingDTO;
-import biblivre.core.exceptions.DAOException;
+import biblivre.core.PreparedStatementUtil;
 
 public class RequestDAO extends AbstractDAO {
+
+	private static final String _UPDATE_STATUS_SQL =
+		"UPDATE requests" +
+		"SET status = ? " +
+		"WHERE id IN (" +
+			"SELECT r.id FROM requests r " +
+			"INNER JOIN request_quotation rq ON rq.request_id = r.id " +
+			"INNER JOIN quotation q ON q.id = rq.quotation_id " +
+			"INNER JOIN order o ON o.quotation_id = q.id " +
+			"WHERE o.id = ?" +
+		"); ";
+
+	private static final String _UPDATE_SQL =
+		"UPDATE requests" +
+		"SET author = ?, item_title = ?, item_subtitle = ?, " +
+			"edition_number = ?, publisher = ?, info = ?, requester = ?, " +
+			"quantity = ?, modified = now(), modified_by = ? " +
+		"WHERE id = ?;";
+
+	private static final String _SEARCH_ALL_SQL =
+		"SELECT * FROM requests ORDER BY id ASC LIMIT ? OFFSET ?";
+
+	private static final String _SEARCH_KEYWORD_SQL =
+		"SELECT * FROM requests " +
+		"WHERE requester ilike ? OR author ilike ? OR item_title ilike " +
+		"ORDER BY id ASC LIMIT ? OFFSET ?";
+
+	private static final String _SAVE_SQL =
+		"INSERT INTO requests " +
+		"(requester, author, item_title, item_subtitle, edition_number, " +
+			"publisher, info, status, quantity, created_by) " +
+		"VALUES (" + StringUtils.repeat("?", ", ", 10) + ");";
+
+	private static final String _SAVE_FROM_V3_SQL =
+			"INSERT INTO requests " +
+			"(requester, author, item_title, item_subtitle, edition_number, " +
+				"publisher, info, status, quantity, created_by) " +
+			"VALUES (" + StringUtils.repeat("?", ", ", 10) + ");";
 
 	public static RequestDAO getInstance(String schema) {
 		return (RequestDAO) AbstractDAO.getInstance(RequestDAO.class, schema);
 	}
 
 	public boolean save(RequestDTO dto) {
-		Connection con = null;
-		try {
-			con = this.getConnection();
-
-			StringBuilder sql = new StringBuilder();
-			sql.append("INSERT INTO requests ( ");
-			sql.append("requester, author, item_title, item_subtitle, ");
-			sql.append("edition_number, publisher, info, status, quantity, created_by) ");
-			sql.append("VALUES (");
-			sql.append(StringUtils.repeat("?", ", ", 10));
-			sql.append(");");
-
-			PreparedStatement pstInsert = con.prepareStatement(sql.toString());
-			pstInsert.setString(1, dto.getRequester());
-			pstInsert.setString(2, dto.getAuthor());
-			pstInsert.setString(3, dto.getTitle());
-			pstInsert.setString(4, dto.getSubtitle());
-			pstInsert.setString(5, dto.getEditionNumber());
-			pstInsert.setString(6, dto.getPublisher());
-			pstInsert.setString(7, dto.getInfo());
-			pstInsert.setString(8, dto.getStatus().toString());
-			pstInsert.setInt(9, dto.getQuantity());
-			pstInsert.setInt(10, dto.getCreatedBy());
-
-			return pstInsert.executeUpdate() > 0;
-
-		} catch (Exception e) {
-			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
-		}
+		return executeUpdate(
+			_SAVE_SQL, dto.getRequester(), dto.getAuthor(), dto.getTitle(),
+			dto.getSubtitle(), dto.getEditionNumber(), dto.getPublisher(),
+			dto.getInfo(), dto.getStatus().toString(), dto.getQuantity(),
+			dto.getCreatedBy());
 	}
 
 	public boolean saveFromBiblivre3(List<? extends AbstractDTO> dtoList) {
-		Connection con = null;
-		try {
-			con = this.getConnection();
-
-			StringBuilder sql = new StringBuilder();
-			sql.append("INSERT INTO requests ( ");
-			sql.append("requester, author, item_title, item_subtitle, ");
-			sql.append("edition_number, publisher, info, status, quantity, created_by, id) ");
-			sql.append("VALUES (");
-			sql.append(StringUtils.repeat("?", ", ", 11));
-			sql.append(");");
-
-			PreparedStatement pstInsert = con.prepareStatement(sql.toString());
-
+		return executeQuery(pst -> {
 			for (AbstractDTO abstractDto : dtoList) {
 				RequestDTO dto = (RequestDTO) abstractDto;
-				pstInsert.setString(1, dto.getRequester());
-				pstInsert.setString(2, dto.getAuthor());
-				pstInsert.setString(3, dto.getTitle());
-				pstInsert.setString(4, dto.getSubtitle());
-				pstInsert.setString(5, dto.getEditionNumber());
-				pstInsert.setString(6, dto.getPublisher());
-				pstInsert.setString(7, dto.getInfo());
-				pstInsert.setString(8, dto.getStatus().toString());
-				pstInsert.setInt(9, dto.getQuantity());
-				pstInsert.setInt(10, dto.getCreatedBy());
-				pstInsert.setInt(11, dto.getId());
-				pstInsert.addBatch();
+
+				PreparedStatementUtil.setAllParameters(
+					pst, dto.getRequester(), dto.getAuthor(), dto.getTitle(),
+					dto.getSubtitle(), dto.getEditionNumber(),
+					dto.getPublisher(), dto.getInfo(),
+					dto.getStatus().toString(), dto.getQuantity(),
+					dto.getCreatedBy(), dto.getId()
+				);
+
+				pst.addBatch();
 			}
 
-			pstInsert.executeBatch();
+			pst.executeBatch();
 
-		} catch (Exception e) {
-			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
-		}
-		return true;
+			return true;
+		}, _SAVE_FROM_V3_SQL);
 	}
 
 	public RequestDTO get(int id) {
-		Connection con = null;
-		try {
-			con = this.getConnection();
-
-			StringBuilder sql = new StringBuilder();
-			sql.append("SELECT * FROM requests ");
-			sql.append("WHERE id = ?;");
-
-			PreparedStatement pst = con.prepareStatement(sql.toString());
-			pst.setInt(1, id);
-
-			ResultSet rs = pst.executeQuery();
-			if (rs.next()) {
-				return this.populateDto(rs);
-			}
-		} catch (Exception e) {
-			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
-		}
-		return null;
+		return fetchOne(
+			this::populateDto, "SELECT * FROM requests WHERE id = ?;", id);
 	}
 
-	public DTOCollection<RequestDTO> search(String value, int limit, int offset) {
-		DTOCollection<RequestDTO> list = new DTOCollection<RequestDTO>();
+	public DTOCollection<RequestDTO> search(
+		String value, int limit, int offset) {
 
-		Connection con = null;
-		try {
-			con = this.getConnection();
+		boolean isNotBlank = StringUtils.isNotBlank(value);
 
-			StringBuilder sql = new StringBuilder("SELECT * FROM requests ");
-			if (StringUtils.isNotBlank(value)) {
-				sql.append("WHERE requester ilike ? ");
-				sql.append("OR author ilike ? ");
-				sql.append("OR item_title ilike ? ");
-			}
-			sql.append("ORDER BY id ASC LIMIT ? OFFSET ? ");
-
-			PreparedStatement pst = con.prepareStatement(sql.toString());
-			int i = 1;
-			if (StringUtils.isNotBlank(value)) {
-				pst.setString(i++, "%" + value + "%");
-				pst.setString(i++, "%" + value + "%");
-				pst.setString(i++, value);
-			}
-			pst.setInt(i++, limit);
-			pst.setInt(i++, offset);
-
-			StringBuilder sqlCount = new StringBuilder("SELECT count(*) as total FROM requests ");
-			if (StringUtils.isNotBlank(value)) {
-				sqlCount.append("WHERE requester ilike ? ");
-				sqlCount.append("OR author ilike ? ");
-				sqlCount.append("OR item_title ilike ? ");
-			}
-
-			PreparedStatement pstCount = con.prepareStatement(sqlCount.toString());
-			if (StringUtils.isNotBlank(value)) {
-				pstCount.setString(1, "%" + value + "%");
-				pstCount.setString(2, "%" + value + "%");
-				pstCount.setString(3, value);
-			}
-
-			ResultSet rs = pst.executeQuery();
-			while (rs.next()) {
-				list.add(this.populateDto(rs));
-			}
-
-			ResultSet rsCount = pstCount.executeQuery();
-			if (rsCount.next()) {
-				int total = rsCount.getInt("total");
-
-				PagingDTO paging = new PagingDTO(total, limit, offset);
-				list.setPaging(paging);
-			}
-
-		} catch (Exception e) {
-			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
+		if (isNotBlank) {
+			return pagedListWith(
+				this::populateDto, _SEARCH_KEYWORD_SQL, limit, offset, value);
 		}
+		else {
+			String likeValue = "%" + value + "%";
 
-		return list;
+			return pagedListWith(
+				this::populateDto, _SEARCH_ALL_SQL, limit, offset, likeValue,
+				likeValue, "%", value);
+		}
 	}
 
 	public boolean update(RequestDTO dto) {
-		Connection con = null;
-		try {
-			con = this.getConnection();
-
-			StringBuilder sql = new StringBuilder();
-			sql.append("UPDATE requests ");
-			sql.append("SET author = ?, item_title = ?, item_subtitle = ?, edition_number = ?, ");
-			sql.append("publisher = ?, info = ?, ");
-			sql.append("requester = ?, quantity = ?, modified = now(), modified_by = ? ");
-			sql.append("WHERE id = ?;");
-
-			PreparedStatement pst = con.prepareStatement(sql.toString());
-			pst.setString(1, dto.getAuthor());
-			pst.setString(2, dto.getTitle());
-			pst.setString(3, dto.getSubtitle());
-			pst.setString(4, dto.getEditionNumber());
-			pst.setString(5, dto.getPublisher());
-			pst.setString(6, dto.getInfo());
-			pst.setString(7, dto.getRequester());
-			pst.setInt(8, dto.getQuantity());
-			pst.setInt(9, dto.getModifiedBy());
-			pst.setInt(10, dto.getId());
-			return pst.executeUpdate() > 0;
-
-		} catch (Exception e) {
-			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
-		}
-
+		return executeUpdate(
+			_UPDATE_SQL, dto.getAuthor(), dto.getTitle(), dto.getSubtitle(),
+			dto.getEditionNumber(), dto.getPublisher(), dto.getInfo(),
+			dto.getRequester(), dto.getQuantity(), dto.getModifiedBy(),
+			dto.getId());
 	}
 
 	public boolean updateRequestStatus(int orderId, RequestStatus status) {
-		Connection con = null;
-		try {
-			con = this.getConnection();
-
-			StringBuilder sql = new StringBuilder();
-			sql.append("UPDATE requests SET status = ? ");
-			sql.append("WHERE id IN (");
-			sql.append("SELECT r.id FROM requests r ");
-			sql.append("INNER JOIN request_quotation rq ");
-			sql.append("ON rq.request_id = r.id ");
-			sql.append("INNER JOIN quotation q ");
-			sql.append("ON q.id = rq.quotation_id ");
-			sql.append("INNER JOIN order o ");
-			sql.append("ON o.quotation_id = q.id ");
-			sql.append("WHERE o.id = ?); ");
-
-			PreparedStatement pst = con.prepareStatement(sql.toString());
-			pst.setString(1, status.toString());
-			pst.setInt(2, orderId);
-
-			return pst.executeUpdate() > 0;
-
-		} catch (Exception e) {
-			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
-		}
+		return executeUpdate(_UPDATE_STATUS_SQL, status.toString(), orderId);
 	}
-
 
 	public boolean delete(RequestDTO dto) {
-		Connection con = null;
-		try {
-			con = this.getConnection();
-
-			StringBuilder sql = new StringBuilder();
-			sql.append("DELETE FROM requests ");
-			sql.append("WHERE id = ?; ");
-
-			PreparedStatement pstInsert = con.prepareStatement(sql.toString());
-			pstInsert.setInt(1, dto.getId());
-
-			return pstInsert.executeUpdate() > 0;
-
-		} catch (Exception e) {
-			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
-		}
-
+		return executeUpdate("DELETE FROM requests WHERE id = ?", dto.getId());
 	}
-
 
 	private RequestDTO populateDto(ResultSet rs) throws Exception {
 		RequestDTO dto = new RequestDTO();
