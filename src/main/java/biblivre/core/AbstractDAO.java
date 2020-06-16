9c929cc36567742fd79d9c9170f02cfbb1ba8b5a
Cleydyr Albuquerque <cleydyr@gmail.com>
2020-06-16 12:17:33 -0300

    refine

--- src/main/java/biblivre/core/AbstractDAO.java
+++ src/main/java/biblivre/core/AbstractDAO.java
@@ -37,7 +37,6 @@ import java.util.function.Function;
 
 import javax.sql.DataSource;
 
-import org.apache.commons.lang3.StringUtils;
 import org.postgresql.PGConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,63 +44,51 @@ import org.slf4j.LoggerFactory;
 import biblivre.core.exceptions.DAOException;
 import biblivre.core.utils.CheckedConsumer;
 import biblivre.core.utils.CheckedFunction;
-import biblivre.core.utils.Constants;
-import biblivre.core.utils.Pair;
 
 public abstract class AbstractDAO {
-	private static Map<String, DataSource> dataSourceMap = new HashMap<String, DataSource>();
+	private static final String _CHECK_TABLE_EXISTENCE_SQL =
+		"SELECT count(*) as count " +
+		"FROM information_schema.tables " +
+		"WHERE table_schema = ? and table_name = ?";
 
-	protected Logger logger = LoggerFactory.getLogger(this.getClass());
-	private String schema;
-	private String dataSourceName;
+	private static final String _CHECK_COLUMN_EXISTENCE_SQL =
+		"SELECT count(*) as count " +
+		"FROM information_schema.columns " +
+		"WHERE table_schema = ? and table_name = ? and column_name = ?";
+
+	private static final String _FIX_SEQUENCE_SQL_TPL =
+		"SELECT setval('%s', coalesce((SELECT max(%s) + 1 FROM %s), 1), false)";
+
+	private static final String _NEXT_SERIAL_SQL_TPL =
+		"SELECT nextval('%s') FROM %s";
+
+	private static Map<String, DataSource> dataSourceMap = new HashMap<>();
+
+	protected static final Logger logger = LoggerFactory.getLogger(AbstractDAO.class);
 
 	protected static CheckedFunction<PreparedStatement, Boolean> EXECUTE =
 			pst -> pst.execute();
 
-	private static HashMap<Pair<Class<? extends AbstractDAO>, String>, AbstractDAO> instances = new HashMap<Pair<Class<? extends AbstractDAO>, String>, AbstractDAO>();
+	private static HashMap<Class<? extends AbstractDAO>, AbstractDAO> INSTANCES =
+		new HashMap<>();
+
+	private DataSource datasource;
 
 	protected AbstractDAO() {
 	}
 
-	protected static AbstractDAO getInstance(Class<? extends AbstractDAO> cls, String schema) {
-		return AbstractDAO.getInstance(cls, schema, "biblivre4");
-	}
-
-	protected static AbstractDAO getInstance(Class<? extends AbstractDAO> cls, String schema, String dataSourceName) {
-		Pair<Class<? extends AbstractDAO>, String> pair = new Pair<Class<? extends AbstractDAO>, String>(cls, schema + ":" + dataSourceName);
-		AbstractDAO instance = AbstractDAO.instances.get(pair);
-
-		if (instance == null) {
-			if (!AbstractDAO.class.isAssignableFrom(cls)) {
-				throw new IllegalArgumentException("DAO: getInstance: Class " + cls.getName() + " is not a subclass of DAO.");
-			}
-
+	@SuppressWarnings("unchecked")
+	protected static <T extends AbstractDAO> T getInstance(Class<T> cls) {
+		AbstractDAO instance = INSTANCES.computeIfAbsent(cls, __ -> {
 			try {
-				instance = cls.newInstance();
-				instance.setSchema(schema);
-				instance.setDataSourceName(dataSourceName);
+				return cls.newInstance();
+			} catch (InstantiationException | IllegalAccessException e) {
+				logger.error(e.getMessage(), e);
+			}
+			return null;
+		});
 
-				AbstractDAO.instances.put(pair, instance);
-			} catch(Exception ex) {}
-		}
-
-		return instance;
-	}
-
-	public void setSchema(String schema) {
-		this.schema = schema;
-	}
-
-	public String getSchema() {
-		return StringUtils.defaultString(this.schema, Constants.GLOBAL_SCHEMA);
-	}
-
-	public String getDataSourceName() {
-		return this.dataSourceName;
-	}
-
-	public void setDataSourceName(String dataSourceName) {
-		this.dataSourceName = dataSourceName;
+		return (T) instance;
 	}
 
 	public static Map<String, DataSource> getDataSourceMap() {
@@ -112,43 +99,34 @@ public abstract class AbstractDAO {
 		AbstractDAO.dataSourceMap = dataSourceMap;
 	}
 
-	public boolean isGlobalSchema() {
-		return this.getSchema().equals(Constants.GLOBAL_SCHEMA);
+	protected Connection getConnection() throws SQLException {
+		return this.getDataSource().getConnection();
 	}
 
-	protected final Connection getConnection() throws SQLException {
-		Connection con = this.getDataSource().getConnection();
+	protected final Connection getConnection(Context ctx) throws SQLException {
+		Connection con = this.getConnection();
 
-		/*
-		DatabaseMetaData dbmd = con.getMetaData();
-		System.out.println("=====  Driver info =====");
-		System.out.println("DriverName: " + dbmd.getDriverName() );
-		System.out.println("DriverVersion: " + dbmd.getDriverVersion() );
-		System.out.println("DriverMajorVersion: " + dbmd.getDriverMajorVersion());
-		*/
-
-		if (this.getSchema() != null) {
-			con.createStatement().execute("SET search_path = '" + this.getSchema() + "', public, pg_catalog;");
+		try (Statement st = con.createStatement()) {
+			st.execute(
+				String.format(
+					"SET search_path = '%s', public, pg_catalog",
+					ctx.getSchema()));
 		}
 
 		return con;
 	}
 
 	private final DataSource getDataSource() {
-		DataSource ds = dataSourceMap.get(this.getDataSourceName());
-
-		if (ds == null) {
-			ds = HikariDataSourceConnectionProvider.getDataSource();
+		if (datasource == null) {
+			datasource = HikariDataSourceConnectionProvider.getDataSource();
 		}
 
-		if (ds == null) {
-			this.logger.error("[DAO.Constructor] Data Source not found.");
+		if (datasource == null) {
+			logger.error("[DAO.Constructor] Data Source not found.");
 			throw new RuntimeException("Data Source not found!");
-		} else {
-			dataSourceMap.put(this.getDataSourceName(), ds);
 		}
 
-		return ds;
+		return datasource;
 	}
 
 	protected final void closeConnection(Connection con) {
@@ -181,37 +159,25 @@ public abstract class AbstractDAO {
 		}
 	}
 
-	public final Integer getNextSerial(String sequence) {
-		Integer serial = 0;
-		Connection con = null;
+	public final Integer getNextSerial(Connection con, String sequence) {
+		String sql = String.format(_NEXT_SERIAL_SQL_TPL, sequence, sequence);
 
-		try {
-			con = this.getConnection();
+		try (PreparedStatement pst = con.prepareStatement(sql);
+			ResultSet rs = pst.executeQuery()) {
 
-			String sql = "SELECT nextval('" + sequence + "') FROM " + sequence;
-			PreparedStatement pst = con.prepareStatement(sql);
-
-			ResultSet rs = pst.executeQuery();
-
-			if ((rs != null) && rs.next()) {
-				serial = rs.getInt(1);
-			}
+			return rs.getInt(1);
 		} catch (Exception e) {
 			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
 		}
-
-		return serial;
 	}
 
 	public final void fixSequence(String sequence, String tableName, String tableIdColumnName) {
 		Connection con = null;
+		String sql = String.format(_FIX_SEQUENCE_SQL_TPL, sequence, tableIdColumnName, tableName);
 
 		try {
 			con = this.getConnection();
 
-			String sql = "SELECT setval('" + sequence + "', coalesce((SELECT max(" + tableIdColumnName + ") + 1 FROM " + tableName + "), 1), false);";
 			PreparedStatement pst = con.prepareStatement(sql);
 
 			pst.executeQuery();
@@ -248,77 +214,42 @@ public abstract class AbstractDAO {
 		}
 	}
 
-	public final boolean checkColumnExistance(String tableName, String columnName) throws SQLException {
-		Connection con = null;
+	public final boolean checkColumnExistence(
+		Context ctx, String tableName, String columnName)
+		throws SQLException {
 
-		try {
-			con = this.getConnection();
-
-			String sql = "SELECT count(*) as count FROM information_schema.columns WHERE table_schema = ? and table_name = ? and column_name = ?;";
-
-			PreparedStatement pst = con.prepareStatement(sql);
-			pst.setString(1, this.getSchema());
-			pst.setString(2, tableName);
-			pst.setString(3, columnName);
-
-			ResultSet rs = pst.executeQuery();
-			return rs.next() && rs.getInt("count") == 1;
-		} finally {
-			this.closeConnection(con);
-		}
+		return fetchOne(
+			rs -> rs.getInt("count") == 1, _CHECK_COLUMN_EXISTENCE_SQL,
+			ctx.getSchema(), tableName, columnName);
 	}
 
+	public final boolean checkTableExistance(Context ctx, String tableName)
+		throws SQLException {
 
-	public final boolean checkTableExistance(String tableName) throws SQLException {
-		Connection con = null;
-
-		try {
-			con = this.getConnection();
-
-			String sql = "SELECT count(*) as count FROM information_schema.tables WHERE table_schema = ? and table_name = ?;";
-
-			PreparedStatement pst = con.prepareStatement(sql);
-			pst.setString(1, this.getSchema());
-			pst.setString(2, tableName);
-
-			ResultSet rs = pst.executeQuery();
-			return rs.next() && rs.getInt("count") == 1;
-		} finally {
-			this.closeConnection(con);
-		}
+		return fetchOne(
+			rs -> rs.getInt("count") == 1, _CHECK_TABLE_EXISTENCE_SQL,
+			ctx.getSchema(), tableName);
 	}
 
 
 	public final String getPostgreSQLVersion() throws SQLException {
-		Connection con = null;
-
-		try {
-			con = this.getConnection();
-
-			String sql = "SELECT version() as version;";
-
-			Statement st = con.createStatement();
-
-			ResultSet rs = st.executeQuery(sql);
-
-			if (rs.next()) {
-				return rs.getString("version");
-			}
-
-			return "";
-		} finally {
-			this.closeConnection(con);
-		}
+		return fetchOne(
+			rs -> rs.getString("version"), "SELECT version() as version");
 	}
 
-	protected final boolean hasColumn(ResultSet rs, String columnName) throws SQLException {
+	protected final boolean hasColumn(ResultSet rs, String columnName)
+		throws SQLException {
+
 	    ResultSetMetaData metadata = rs.getMetaData();
+
 	    int columns = metadata.getColumnCount();
-	    for (int x = 1; x <= columns; x++) {
-	        if (columnName.equals(metadata.getColumnName(x))) {
+
+	    for (int i = 1; i <= columns; i++) {
+	        if (columnName.equals(metadata.getColumnName(i))) {
 	            return true;
 	        }
 	    }
+
 	    return false;
 	}
 
@@ -330,7 +261,7 @@ public abstract class AbstractDAO {
 
 			return pgcon;
 		} catch (Exception e) {
-			this.logger.info("getInnermostDelegate Unwrap");
+			logger.info("getInnermostDelegate Unwrap");
 			e.printStackTrace();
 		}
 
@@ -340,7 +271,7 @@ public abstract class AbstractDAO {
 
 			return pgcon;
 		} catch (Exception e) {
-			this.logger.info("Skipping org.apache.tomcat.dbcp.dbcp.DelegatingConnection");
+			logger.info("Skipping org.apache.tomcat.dbcp.dbcp.DelegatingConnection");
 
 			e.printStackTrace();
 		}
@@ -351,7 +282,7 @@ public abstract class AbstractDAO {
 
 			return pgcon;
 		} catch (Exception e) {
-			this.logger.info("org.apache.commons.dbcp.DelegatingConnection");
+			logger.info("org.apache.commons.dbcp.DelegatingConnection");
 
 			e.printStackTrace();
 		}
@@ -396,10 +327,10 @@ public abstract class AbstractDAO {
 	}
 
 	public <T> T executeQuery(
-		CheckedFunction<PreparedStatement, T> f, String sql)
+			Context ctx, CheckedFunction<PreparedStatement, T> f, String sql)
 		throws DAOException {
 
-		try (Connection con = this.getConnection();
+		try (Connection con = this.getConnection(ctx);
 			PreparedStatement pst = con.prepareStatement(sql)) {
 
 			return f.apply(pst);
@@ -409,11 +340,11 @@ public abstract class AbstractDAO {
 	}
 
 	public <T> T executeQuery(
-		CheckedFunction<PreparedStatement, T> f, String sql,
+		Context ctx, CheckedFunction<PreparedStatement, T> f, String sql,
 		Object... parameters)
 		throws DAOException {
 
-		try (Connection con = this.getConnection();
+		try (Connection con = this.getConnection(ctx);
 			PreparedStatement pst = con.prepareStatement(sql)) {
 
 			PreparedStatementUtil.setAllParameters(pst, parameters);
@@ -424,61 +355,69 @@ public abstract class AbstractDAO {
 		}
 	}
 
-	public boolean executeUpdate(String sql, Object... parameters) {
-		return executeQuery(pst -> pst.executeUpdate() > 0, sql, parameters);
+	public boolean executeUpdate(
+		Context ctx, String sql, Object... parameters) {
+
+		return executeQuery(
+			ctx, pst -> pst.executeUpdate() > 0, sql, parameters);
 	}
 
 	public <T> boolean executeBatchUpdate(
-		CheckedBiConsumer<PreparedStatement, T> consumer, Collection<T> items,
-		String sql) {
+		Context ctx, CheckedBiConsumer<PreparedStatement, T> consumer,
+		Collection<T> items, String sql) {
 
-		return executeQuery(pst -> {
-			for (T item : items) {
-				consumer.accept(pst, item);
-
-				pst.addBatch();
-			}
-
-			pst.executeBatch();
-
-			return true;
-		}, sql);
+		return executeQuery(ctx,
+			pst -> {
+				for (T item : items) {
+					consumer.accept(pst, item);
+	
+					pst.addBatch();
+				}
+	
+				pst.executeBatch();
+	
+				return true;
+			},
+			sql);
 	}
 
 	@SafeVarargs
 	public final <T> boolean executeBatchUpdate(
-		Collection<?> items, Class<T> target, String sql,
+		Context ctx, Collection<?> items, Class<T> target, String sql,
 		Function<T, ?>... fs) {
 
-			return executeQuery(pst -> {
-				for (Object item : items) {
-					T targetItem = target.cast(item);
-
-					List<Object> parameters = new ArrayList<>();
-
-					for (Function<T, ?> f : fs) {
-						parameters.add(f.apply(targetItem));
+			return executeQuery(ctx,
+				pst -> {
+					for (Object item : items) {
+						T targetItem = target.cast(item);
+	
+						List<Object> parameters = new ArrayList<>();
+	
+						for (Function<T, ?> f : fs) {
+							parameters.add(f.apply(targetItem));
+						}
+	
+						PreparedStatementUtil.setAllParameters(
+							pst, parameters.toArray());
+	
+						pst.addBatch();
 					}
-
-					PreparedStatementUtil.setAllParameters(
-						pst, parameters.toArray());
-
-					pst.addBatch();
-				}
-
-				pst.executeBatch();
-
-				return true;
-			}, sql);
+	
+					pst.executeBatch();
+	
+					return true;
+				},
+				sql);
 		}
 
 	public <T extends AbstractDTO> DTOCollection<T> pagedListWith(
-		CheckedFunction<ResultSet, T> mapper, String sql, int limit, int offset,
-		Object... parameters) {
+		Context ctx, CheckedFunction<ResultSet, T> mapper, String sql,
+		int limit, int offset, Object... parameters) {
 
 		DTOCollection<T> list = new DTOCollection<>();
 
-		executeQuery(pst -> {
+		executeQuery(ctx,
+			pst -> {
 			Object[] newParameters = new Object[parameters.length + 2];
 
 			System.arraycopy(
@@ -502,32 +441,35 @@ public abstract class AbstractDAO {
 			.substring(0, sql.lastIndexOf("ORDER BY"))
 			.replace("*", "count(*)");
 
-		executeQuery(pst -> {
-			PreparedStatementUtil.setAllParameters(pst, parameters);
-
-			try (ResultSet rs = pst.executeQuery()) {
-				rs.next();
-
-				int count = rs.getInt(1);
-
-				PagingDTO paging = new PagingDTO(count, limit, offset);
-
-				list.setPaging(paging);
-			}
-
-			return null;
-		}, countSql);
+		executeQuery(ctx,
+			pst -> {
+				PreparedStatementUtil.setAllParameters(pst, parameters);
+	
+				try (ResultSet rs = pst.executeQuery()) {
+					rs.next();
+	
+					int count = rs.getInt(1);
+	
+					PagingDTO paging = new PagingDTO(count, limit, offset);
+	
+					list.setPaging(paging);
+				}
+	
+				return null;
+			},
+			countSql);
 
 		return list;
 	}
 
 	public <T> List<T> listWith(
-		CheckedFunction<ResultSet, T> mapper, String sql, Object... parameters)
+		Context ctx, CheckedFunction<ResultSet, T> mapper, String sql,
+		Object... parameters)
 		throws DAOException {
 
 		List<T> list = new ArrayList<>();
 
-		try (Connection con = this.getConnection();
+		try (Connection con = this.getConnection(ctx);
 			PreparedStatement pst = con.prepareStatement(sql)) {
 
 			PreparedStatementUtil.setAllParameters(pst, parameters);
@@ -544,12 +486,13 @@ public abstract class AbstractDAO {
 		}
 	}
 
-	public final void onTransactionContext(CheckedConsumer<Connection> consumer) {
+	public final void onTransactionContext(
+		Context ctx, CheckedConsumer<Connection> consumer) {
 
 		Connection con = null;
 
 		try {
-			con  = this.getConnection();
+			con  = this.getConnection(ctx);
 
 			con.setAutoCommit(false);
 
@@ -563,5 +506,4 @@ public abstract class AbstractDAO {
 			this.closeConnection(con);
 		}
 	}
-
 }
