9669609c9f6fa4bb2aaed670b5c55efe4cfff590
Cleydyr Albuquerque <cleydyr@gmail.com>
2020-06-16 15:18:48 -0300

    correct implementation of context thread local

--- src/main/java/biblivre/core/ContextThreadLocal.java
+++ src/main/java/biblivre/core/ContextThreadLocal.java
@@ -3,15 +3,15 @@ package biblivre.core;
 public class ContextThreadLocal {
 	private static ThreadLocal<Context> THREADLOCAL;
 
-	public static void init() {
-		THREADLOCAL = new ThreadLocal<>();
+	public static void init(Context ctx) {
+		THREADLOCAL = ThreadLocal.withInitial(() -> ctx);
 	}
 
 	public static Context getContext() {
 		return THREADLOCAL.get();
 	}
 
-	public void cleanUp() {
+	public static void cleanUp() {
 		THREADLOCAL.remove();
 	}
 }
