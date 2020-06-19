e315fd04ebea699d3036846883343f083d0004c8
Cleydyr Albuquerque <cleydyr@gmail.com>
2020-06-18 21:43:10 -0300

    use convenience methods

--- src/main/java/biblivre/digitalmedia/DigitalMediaDAO.java
+++ src/main/java/biblivre/digitalmedia/DigitalMediaDAO.java
@@ -23,10 +23,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.postgresql.PGConnection;
@@ -40,27 +38,32 @@ import biblivre.core.file.MemoryFile;
 
 public class DigitalMediaDAO extends AbstractDAO implements IDigitalMediaDAO {
 
+	private static final String _LIST_SQL =
+		"SELECT id, blob, name FROM digital_media";
+
+	private static final String _DELETE_SQL =
+		"DELETE FROM digital_media WHERE id = ?";
+
+	private static final String _LOAD_SQL =
+		"SELECT name, blob, content_type, size, created "
+		+ "FROM digital_media "
+		+ "WHERE id = ? AND name = ?";
+
+	private static final String _SAVE_SQL =
+		"INSERT INTO digital_media "
+		+ "(id, name, blob, content_type, size) "
+		+ "VALUES (?, ?, ?, ?, ?)";
+
 	@Override
 	public long createOID() {
-		Connection con = null;
-		try {
-			con = this.getConnection();
-			con.setAutoCommit(false);
-
+		return onTransactionContext(con -> {
 			PGConnection pgcon = getPGConnection(con);
 
 			LargeObjectManager lobj = pgcon.getLargeObjectAPI();
 			long oid = lobj.createLO();
 
-			this.commit(con);
-
 			return oid;
-		} catch (Exception e) {
-			this.rollback(con);
-			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
-		}
+		});
 	}
 
 	@Override
@@ -74,42 +77,37 @@ public class DigitalMediaDAO extends AbstractDAO implements IDigitalMediaDAO {
 				}
 
 				Integer serial = file.getId();
+
 				if (serial == null) {
 					serial = getNextSerial(con, "digital_media_id_seq");
+
 					file.setId(serial);
 				}
 
 				if (serial != 0) {
 					LargeObjectManager lobj = pgcon.getLargeObjectAPI();
+
 					long oid = lobj.createLO();
 
 					LargeObject obj = lobj.open(oid, LargeObjectManager.WRITE);
 
 					byte buf[] = new byte[4096];
+
 					int bytesRead = 0;
+
 					while ((bytesRead = is.read(buf)) > 0) {
 						obj.write(buf, 0, bytesRead);
 					}
 
 					obj.close();
 
-					String sql = "INSERT INTO digital_media (id, name, blob, content_type, size) VALUES (?, ?, ?, ?, ?);";
+					executeUpdate(
+						_SAVE_SQL, serial, file.getName(), oid,
+						file.getContentType(), file.getSize());
 
-					PreparedStatement pst = con.prepareStatement(sql);
-					pst.setInt(1, serial);
-					pst.setString(2, file.getName());
-					pst.setLong(3, oid);
-					pst.setString(4, file.getContentType());
-					pst.setLong(5, file.getSize());
-
-					pst.executeUpdate();
-					pst.close();
 					file.close();
-
-					this.commit(con);
-				} else {
-					this.rollback(con);
 				}
+
 				return serial;
 			}
 		});
@@ -156,13 +154,7 @@ public class DigitalMediaDAO extends AbstractDAO implements IDigitalMediaDAO {
 
 	@Override
 	public final DatabaseFile load(int id, String name) {
-		Connection con = null;
-		DatabaseFile file = null;
-
-		try {
-			con = this.getConnection();
-			con.setAutoCommit(false);
-
+		return onTransactionContext(con -> {
 			PGConnection pgcon = this.getPGConnection(con);
 
 			if (pgcon == null) {
@@ -171,87 +163,46 @@ public class DigitalMediaDAO extends AbstractDAO implements IDigitalMediaDAO {
 
 			LargeObjectManager lobj = pgcon.getLargeObjectAPI();
 
-			StringBuilder sql = new StringBuilder();
 			// We check both ID and FILE_NAME for security reasons, so users can't "guess"
 			// id's and get the files.
-			sql.append("SELECT name, blob, content_type, size, created FROM digital_media ");
-			sql.append("WHERE id = ? AND name = ?;");
 
-			PreparedStatement pst = con.prepareStatement(sql.toString());
-			pst.setInt(1, id);
-			pst.setString(2, name);
+			return fetchOne(rs -> {
+				DatabaseFile file = null;
 
-			ResultSet rs = pst.executeQuery();
-			if (rs.next()) {
-				long oid = rs.getLong("blob");
-				LargeObject obj = lobj.open(oid, LargeObjectManager.READ);
+				if (rs.next()) {
+					long oid = rs.getLong("blob");
+					LargeObject obj = lobj.open(oid, LargeObjectManager.READ);
 
-				file = new DatabaseFile(con, obj);
+					file = new DatabaseFile(con, obj);
 
-				file.setName(rs.getString("name"));
-				file.setContentType(rs.getString("content_type"));
-				file.setLastModified(rs.getTimestamp("created").getTime());
-				file.setSize(rs.getLong("size"));
-			}
-		} catch (Exception e) {
-			this.rollback(con);
-			this.closeConnection(con);
+					file.setName(rs.getString("name"));
+					file.setContentType(rs.getString("content_type"));
+					file.setLastModified(rs.getTimestamp("created").getTime());
+					file.setSize(rs.getLong("size"));
+				}
 
-			throw new DAOException(e);
-		} finally {
-			// We must leave this connection open. file.close() will close it when needed.
-		}
-
-		return file;
+				return file;
+			}, _LOAD_SQL, id, name);
+		});
 	}
 
 	@Override
 	public boolean delete(int id) {
-		Connection con = null;
-		try {
-			con = this.getConnection();
-			StringBuilder sql = new StringBuilder();
-			// We check both ID and FILE_NAME for security reasons, so users can't "guess"
-			// id's and get the files.
-			sql.append("DELETE FROM digital_media ");
-			sql.append("WHERE id = ?;");
-
-			PreparedStatement pst = con.prepareStatement(sql.toString());
-			pst.setInt(1, id);
-
-			int deleted = pst.executeUpdate();
-			// Find out if we need to check how many records were deleted from DB.
-			return deleted > 0;
-		} catch (Exception e) {
-			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
-		}
+		return executeUpdate(_DELETE_SQL, id);
 	}
 
 	@Override
 	public List<DigitalMediaDTO> list() {
-		Connection con = null;
-		List<DigitalMediaDTO> list = new ArrayList<DigitalMediaDTO>();
-		try {
-			con = this.getConnection();
-			Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery("SELECT id, blob, name FROM digital_media;");
-
-			while (rs.next()) {
-				DigitalMediaDTO dto = new DigitalMediaDTO();
-				dto.setId(rs.getInt("id"));
-				dto.setBlob(rs.getLong("blob"));
-				dto.setName(rs.getString("name"));
-				list.add(dto);
-			}
-
-			return list;
-		} catch (Exception e) {
-			throw new DAOException(e);
-		} finally {
-			this.closeConnection(con);
-		}
+		return listWith(this::_populateDTO, _LIST_SQL);
 	}
 
+	private DigitalMediaDTO _populateDTO(ResultSet rs) throws SQLException {
+		DigitalMediaDTO dto = new DigitalMediaDTO();
+
+		dto.setId(rs.getInt("id"));
+		dto.setBlob(rs.getLong("blob"));
+		dto.setName(rs.getString("name"));
+
+		return dto;
+	}
 }
