623b1487a386a0156c963df571ec18ebc1cf446b
Cleydyr Albuquerque <cleydyr@gmail.com>
2021-03-26 14:38:20 -0300

    #105 fix autocompletion for authorities and refactor

--- src/main/java/biblivre/cataloging/RecordDAO.java
+++ src/main/java/biblivre/cataloging/RecordDAO.java
@@ -735,12 +735,14 @@ public abstract class RecordDAO extends AbstractDAO {
 
         RecordDTO rdto = this.createRecord();
 
+        rdto.setId(rs.getInt("record_id"));
         rdto.setIso2709(new String(rs.getBytes("iso2709"), Constants.DEFAULT_CHARSET));
         rdto.setRecord(MarcUtils.iso2709ToRecord(rdto.getIso2709()));
         rdto.setJson(MarcUtils.recordToJson(rdto.getRecord()));
 
         dto.setPhrase(rs.getString("phrase"));
         dto.setRecord(rdto);
+        dto.setRecordId(rdto.getId());
 
         return dto;
     }
