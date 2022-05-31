25dc979f1e9d3fee9e8ea7f2d38cac7d0a0fd9be
Cleydyr de Albuquerque <cleydyr@gmail.com>
2022-05-31 12:22:34 +0200

    wip frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,132 @@
 import React, { useState } from 'react';
-import { EuiButton, EuiComboBox, EuiFlexGroup, EuiPageTemplate, EuiProvider, EuiSearchBar } from '@elastic/eui';
+import { EuiAccordion, EuiBreadcrumbs, EuiButton, EuiComboBox, EuiFlexGroup, EuiPageTemplate, EuiPanel, EuiProvider, EuiSearchBar, EuiSideNav } from '@elastic/eui';
 import '@elastic/eui/dist/eui_theme_light.css';
 import { css, jsx } from '@emotion/react';
 
-export default ({ button = <></>}) => {
+export default ({ button = <EuiButton>Sair</EuiButton> }) => {
   const [showBottomBar, setshowBottomBar] = useState(false);
 
   const style = css``;
 
+  const noop = () => { };
+
   return (
     <EuiPageTemplate
-      pageSideBar={<EuiComboBox></EuiComboBox>}
+      pageSideBar={
+        <EuiSideNav
+          heading="Biblivre 6"
+          mobileTitle="Nav Items"
+          toggleOpenOnMobile={noop}
+          isOpenOnMobile={false}
+          items={[
+            {
+              name: 'Circulação',
+              id: '1',
+              items: [
+                {
+                  name: 'Children only',
+                  id: '1.1',
+                  items: [
+                    {
+                      name: 'General',
+                      id: '1.1.1',
+                    },
+                    {
+                      name: 'Timelion',
+                      id: '1.1.2',
+                    },
+                  ],
+                },
+                {
+                  name: 'Cadastro de usuários',
+                  id: '1',
+                  itens: [
+                    {
+                      name: 'foo0',
+                      id: '1',
+                      forceOpen: true
+                    }
+                  ]
+                },
+                {
+                  name: 'Empréstimos e devoluções',
+                  id: '2'
+                },
+                {
+                  name: 'Reservas',
+                  id: '3'
+                },
+                {
+                  name: 'Controle de acesso',
+                  id: '1'
+                },
+                {
+                  name: 'Impressão de carteirinhas',
+                  id: '1'
+                },
+              ],
+            },
+            {
+              name: 'Circulação',
+              id: '1',
+              items: [
+                {
+                  name: 'Cadastro de usuários',
+                  id: '1',
+                  itens: [
+                    {
+                      name: 'foo0',
+                      id: '1',
+                      forceOpen: true
+                    }
+                  ]
+                },
+                {
+                  name: 'Empréstimos e devoluções',
+                  id: '2'
+                },
+                {
+                  name: 'Reservas',
+                  id: '3'
+                },
+                {
+                  name: 'Controle de acesso',
+                  id: '1'
+                },
+                {
+                  name: 'Impressão de carteirinhas',
+                  id: '1'
+                },
+              ],
+            },
+          ]}
+        />
+      }
       bottomBar={showBottomBar ? 'Bottom bar' : undefined}
       pageHeader={{
-        pageTitle: 'Biblivre 6',
-        rightSideItems: [button],
-        tabs: [
-          { label: 'Tab 1', isSelected: true },
+        breadcrumbs: [
           {
-            label: 'Tab 2',
-            onClick: () => setshowBottomBar((showing) => !showing),
+            text: 'Circulação',
+            href: '#',
+            onClick: (e) => e.preventDefault(),
+          },
+          {
+            text: 'Cartões de acesso',
+            href: '#',
+            onClick: (e) => e.preventDefault(),
           },
         ],
+        pageTitle: 'Cartões de acesso',
+        rightSideItems: [<EuiButton>Adicionar cartão</EuiButton>],
       }}
     >
+      <div>
+        <EuiAccordion id="foobar" buttonContent="Click me to toggle">
+          <EuiPanel color="subdued">
+            Any content inside of <strong>EuiAccordion</strong> will appear here.
+          </EuiPanel>
+        </EuiAccordion>
+      </div>
       <EuiFlexGroup>
         <EuiSearchBar></EuiSearchBar><EuiComboBox></EuiComboBox><EuiButton>Buscar</EuiButton>
       </EuiFlexGroup>
