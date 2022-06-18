c0314dc182a13da96733a74c9fd103cf8db954b2
Cleydyr de Albuquerque <cleydyr@gmail.com>
2022-06-18 15:35:04 +0200

    wip

--- src/main/java/biblivre/controller/AccessCardController.java
+++ src/main/java/biblivre/controller/AccessCardController.java
@@ -1,10 +1,16 @@
 package biblivre.controller;
 
 import biblivre.administration.accesscards.AccessCardBO;
+import biblivre.core.exceptions.ValidationException;
 import biblivre.legacy.entity.AccessCard;
+import biblivre.legacy.repository.AccessCardRepository;
+
+import java.util.Collection;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,19 +18,31 @@ import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class AccessCardController {
-    /*
-     * search
-     * save
-     * delete
-     * update
-     */
-
     @Autowired private AccessCardBO accessCardBO;
+
+    @Autowired private AccessCardRepository accessCardRepository;
 
     @GetMapping("/api/access_card/{id}")
     public AccessCard open(@PathVariable int id) {
         Optional<AccessCard> accessCard = accessCardBO.get(id);
 
         return accessCard.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/api/access_card")
+    public Collection<AccessCard> getAll() {
+        return accessCardRepository.findAll();
+    }
+
+    @DeleteMapping("/api/access_card/{id}")
+    public ResponseEntity<?> delete(@PathVariable int id) {
+        try {
+        	accessCardBO.removeCard(id);
+        }
+        catch (ValidationException validationException) {
+        	return (ResponseEntity<?>) ResponseEntity.notFound();
+        }
+
+        return (ResponseEntity<?>) ResponseEntity.ok();
     }
 }
