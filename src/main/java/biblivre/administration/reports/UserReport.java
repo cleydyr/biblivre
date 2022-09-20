7b7d72fbd2c848a334f13c1556095e4dd7edaa44
Cleydyr de Albuquerque <cleydyr@gmail.com>
2022-09-20 17:44:09 +0200

    fix: add missing component for this report

--- src/main/java/biblivre/administration/reports/UserReport.java
+++ src/main/java/biblivre/administration/reports/UserReport.java
@@ -44,7 +44,9 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserReport extends BaseBiblivreReport {
     public static final DateFormat dd_MM_yyyy = new SimpleDateFormat("dd/MM/yyyy");
     private UserBO userBO;
