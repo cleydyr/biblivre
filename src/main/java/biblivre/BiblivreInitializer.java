96a6108823b0f4b331ece68d7c7f3130f43221f0
Cleydyr de Albuquerque <cleydyr@gmail.com>
2023-01-11 09:47:54 +0100

    build: update deps

--- src/main/java/biblivre/BiblivreInitializer.java
+++ src/main/java/biblivre/BiblivreInitializer.java
@@ -96,8 +96,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
 @SpringBootApplication(
