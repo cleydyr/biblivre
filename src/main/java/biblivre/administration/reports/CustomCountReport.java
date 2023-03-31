6548641ae410def062152f0bf47fed890a037dc3
Cleydyr de Albuquerque <cleydyr@gmail.com>
2023-03-31 21:16:35 +0200

    fix: autowire dependencies and refactor custom report

--- src/main/java/biblivre/administration/reports/CustomCountReport.java
+++ src/main/java/biblivre/administration/reports/CustomCountReport.java
@@ -19,16 +19,34 @@
  ******************************************************************************/
 package biblivre.administration.reports;
 
+import biblivre.administration.indexing.IndexingGroupBO;
 import biblivre.administration.reports.dto.BaseReportDto;
 import biblivre.administration.reports.dto.CustomCountDto;
+import biblivre.cataloging.RecordDTO;
+import biblivre.cataloging.bibliographic.BiblioRecordBO;
+import biblivre.cataloging.enums.RecordDatabase;
+import biblivre.cataloging.enums.RecordType;
+import biblivre.cataloging.search.SearchDTO;
+import biblivre.marc.MarcDataReader;
+import biblivre.marc.MarcUtils;
 import com.lowagie.text.Document;
 import com.lowagie.text.Element;
 import com.lowagie.text.Paragraph;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.apache.commons.lang3.StringUtils;
+import org.marc4j.marc.DataField;
+import org.marc4j.marc.Record;
+import org.marc4j.marc.Subfield;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -38,14 +56,17 @@ public class CustomCountReport extends BaseBiblivreReport {
     private String marcField;
     private String datafield;
     private String subfield;
+    private BiblioRecordBO biblioRecordBO;
+
+    private IndexingGroupBO indexingGroupBO;
 
     @Override
     protected BaseReportDto getReportData(ReportsDTO dto) {
 
-        this.marcField = dto.getMarcField();
-        if (StringUtils.isNotBlank(this.marcField)) {
-            this.datafield = this.marcField.split("\\_")[0];
-            this.subfield = this.marcField.split("\\_")[1];
+        marcField = dto.getMarcField();
+        if (StringUtils.isNotBlank(marcField)) {
+            datafield = marcField.split("\\_")[0];
+            subfield = marcField.split("\\_")[1];
         }
 
         String order = "1";
@@ -53,11 +74,102 @@ public class CustomCountReport extends BaseBiblivreReport {
             order = dto.getCountOrder();
         }
         if (order.equals("2")) {
-            this.index = 1; // field count
+            index = 1; // field count
         } else {
-            this.index = 0; // marc field
+            index = 0; // marc field
         }
-        return reportsBO.getCustomCountData(dto);
+
+        return getCustomCountData(dto);
+    }
+
+    private CustomCountDto getCustomCountData(ReportsDTO reportsDto) {
+        CustomCountDto dto = new CustomCountDto();
+
+        Map<String, Integer> subfieldCounter = new HashMap<>();
+
+        int page = 0;
+        int limit = 100;
+        int offset = limit * page;
+
+        String marcField = reportsDto.getMarcField();
+        String field = marcField.split("_")[0];
+        String subfield = marcField.split("_")[1];
+
+        if (reportsDto.getSearchId() != null && reportsDto.getSearchId() != 0) {
+
+            boolean hasMore = true;
+            while (hasMore) {
+                SearchDTO search = biblioRecordBO.getSearch(reportsDto.getSearchId());
+
+                search.setSort(indexingGroupBO.getDefaultSortableGroupId(RecordType.BIBLIO));
+                search.setIndexingGroup(0);
+                search.getPaging().setRecordsPerPage(limit);
+                search.getPaging().setPage(++page);
+                biblioRecordBO.paginateSearch(search);
+
+                if (search == null || search.size() == 0) {
+                    hasMore = false;
+                } else {
+                    countSubfieldValue(search, subfieldCounter, field, subfield);
+                }
+            }
+
+        } else {
+            RecordDatabase database = reportsDto.getDatabase();
+            if (database == null) {
+                database = RecordDatabase.MAIN;
+            }
+
+            boolean hasMore = true;
+            while (hasMore) {
+                List<RecordDTO> records = biblioRecordBO.list(offset, limit, database);
+                if (records == null || records.size() == 0) {
+                    hasMore = false;
+                } else {
+                    offset = limit * ++page;
+                    countSubfieldValue(records, subfieldCounter, field, subfield);
+                }
+            }
+        }
+
+        List<String[]> data = new ArrayList<String[]>();
+
+        for (Entry<String, Integer> entry : subfieldCounter.entrySet()) {
+            String[] valuePair = new String[2];
+            valuePair[0] = entry.getKey();
+            valuePair[1] = String.valueOf(entry.getValue());
+            data.add(valuePair);
+        }
+
+        dto.setData(data);
+
+        return dto;
+    }
+
+    private void countSubfieldValue(
+            Collection<RecordDTO> records,
+            Map<String, Integer> subfieldCounter,
+            String field,
+            String subfield) {
+
+        for (RecordDTO biblio : records) {
+            Record record = MarcUtils.iso2709ToRecord(biblio.getIso2709());
+            MarcDataReader reader = new MarcDataReader(record);
+            List<DataField> datafields = reader.getDataFields(field);
+
+            for (DataField df : datafields) {
+                List<Subfield> subfields = df.getSubfields(subfield.charAt(0));
+
+                for (Subfield sf : subfields) {
+                    String value = sf.getData();
+                    Integer count = subfieldCounter.get(value);
+                    if (count == null) {
+                        count = 0;
+                    }
+                    subfieldCounter.put(value, ++count);
+                }
+            }
+        }
     }
 
     @Override
@@ -65,17 +177,12 @@ public class CustomCountReport extends BaseBiblivreReport {
             throws Exception {
         CustomCountDto dto = (CustomCountDto) reportData;
         StringBuilder text = new StringBuilder();
-        text.append(this.getText("administration.reports.title.custom_count") + ":\n");
-        text.append(this.datafield).append(" (");
-        text.append(this.getText("marc.bibliographic.datafield." + this.datafield));
-        text.append(")\n$").append(this.subfield);
+        text.append(getText("administration.reports.title.custom_count") + ":\n");
+        text.append(datafield).append(" (");
+        text.append(getText("marc.bibliographic.datafield." + datafield));
+        text.append(")\n$").append(subfield);
         text.append(" (");
-        text.append(
-                this.getText(
-                        "marc.bibliographic.datafield."
-                                + this.datafield
-                                + ".subfield."
-                                + this.subfield));
+        text.append(getText("marc.bibliographic.datafield." + datafield + ".subfield." + subfield));
         text.append(")");
         Paragraph p2 = new Paragraph(text.toString());
         p2.setAlignment(Element.ALIGN_CENTER);
@@ -85,11 +192,7 @@ public class CustomCountReport extends BaseBiblivreReport {
         table.setWidthPercentage(100f);
         createHeader(
                 table,
-                this.getText(
-                        "marc.bibliographic.datafield."
-                                + this.datafield
-                                + ".subfield."
-                                + this.subfield));
+                getText("marc.bibliographic.datafield." + datafield + ".subfield." + subfield));
         Collections.sort(
                 dto.getData(),
                 (o1, o2) -> {
@@ -101,25 +204,24 @@ public class CustomCountReport extends BaseBiblivreReport {
                         return 0;
                     }
 
-                    if (o1[this.index] == null && o2[this.index] == null) {
+                    if (o1[index] == null && o2[index] == null) {
                         return 0;
                     }
 
-                    switch (this.index) {
+                    switch (index) {
                         case 0:
-                            return o1[this.index].compareTo(o2[this.index]);
+                            return o1[index].compareTo(o2[index]);
                         case 1:
-                            return Integer.valueOf(o2[this.index])
-                                    .compareTo(Integer.valueOf(o1[this.index]));
+                            return Integer.valueOf(o2[index]).compareTo(Integer.valueOf(o1[index]));
                         default:
-                            return o1[this.index].compareTo(o2[this.index]);
+                            return o1[index].compareTo(o2[index]);
                     }
                 });
 
         PdfPCell cell;
         int total = 0;
         for (String[] data : dto.getData()) {
-            cell = new PdfPCell(new Paragraph(this.getSmallFontChunk(data[0])));
+            cell = new PdfPCell(new Paragraph(getSmallFontChunk(data[0])));
             cell.setColspan(2);
             cell.setHorizontalAlignment(Element.ALIGN_LEFT);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
@@ -127,19 +229,19 @@ public class CustomCountReport extends BaseBiblivreReport {
             if (data[1] != null && StringUtils.isNumeric(data[1])) {
                 total += Integer.valueOf(data[1]);
             }
-            cell = new PdfPCell(new Paragraph(this.getSmallFontChunk(data[1])));
+            cell = new PdfPCell(new Paragraph(getSmallFontChunk(data[1])));
             cell.setHorizontalAlignment(Element.ALIGN_CENTER);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             table.addCell(cell);
         }
 
         if (total != 0) {
-            cell = new PdfPCell(new Paragraph(this.getBoldChunk("Total")));
+            cell = new PdfPCell(new Paragraph(getBoldChunk("Total")));
             cell.setColspan(2);
             cell.setHorizontalAlignment(Element.ALIGN_LEFT);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             table.addCell(cell);
-            cell = new PdfPCell(new Paragraph(this.getBoldChunk(String.valueOf(total))));
+            cell = new PdfPCell(new Paragraph(getBoldChunk(String.valueOf(total))));
             cell.setHorizontalAlignment(Element.ALIGN_CENTER);
             cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
             table.addCell(cell);
@@ -150,31 +252,35 @@ public class CustomCountReport extends BaseBiblivreReport {
 
     private void createHeader(PdfPTable table, String title) {
         PdfPCell cell;
-        cell = new PdfPCell(new Paragraph(this.getBoldChunk(title)));
-        cell.setBackgroundColor(this.HEADER_BG_COLOR);
+        cell = new PdfPCell(new Paragraph(getBoldChunk(title)));
+        cell.setBackgroundColor(HEADER_BG_COLOR);
         cell.setColspan(2);
-        cell.setBorderWidth(this.HEADER_BORDER_WIDTH);
+        cell.setBorderWidth(HEADER_BORDER_WIDTH);
         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
         table.addCell(cell);
         cell =
                 new PdfPCell(
-                        new Paragraph(
-                                this.getBoldChunk(
-                                        this.getText("administration.reports.field.total"))));
-        cell.setBackgroundColor(this.HEADER_BG_COLOR);
-        cell.setBorderWidth(this.HEADER_BORDER_WIDTH);
+                        new Paragraph(getBoldChunk(getText("administration.reports.field.total"))));
+        cell.setBackgroundColor(HEADER_BG_COLOR);
+        cell.setBorderWidth(HEADER_BORDER_WIDTH);
         cell.setHorizontalAlignment(Element.ALIGN_CENTER);
         cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
         table.addCell(cell);
     }
 
-    public void setReportsBO(ReportsBO reportsBO) {
-        this.reportsBO = reportsBO;
-    }
-
     @Override
     public ReportType getReportType() {
         return ReportType.CUSTOM_COUNT;
+    }
+
+    @Autowired
+    public void setBiblioRecordBO(BiblioRecordBO biblioRecordBO) {
+        this.biblioRecordBO = biblioRecordBO;
+    }
+
+    @Autowired
+    public void setIndexingGroupBO(IndexingGroupBO indexingGroupBO) {
+        this.indexingGroupBO = indexingGroupBO;
     }
 }
--- src/main/java/biblivre/administration/reports/ReportsBO.java
+++ src/main/java/biblivre/administration/reports/ReportsBO.java
@@ -1,38 +1,20 @@
 package biblivre.administration.reports;
 
-import biblivre.administration.indexing.IndexingGroupBO;
-import biblivre.administration.reports.dto.CustomCountDto;
-import biblivre.cataloging.RecordDTO;
-import biblivre.cataloging.bibliographic.BiblioRecordBO;
 import biblivre.cataloging.enums.RecordDatabase;
-import biblivre.cataloging.enums.RecordType;
-import biblivre.cataloging.search.SearchDTO;
 import biblivre.core.AbstractBO;
 import biblivre.core.file.DiskFile;
 import biblivre.core.translations.TranslationsMap;
-import biblivre.marc.MarcDataReader;
-import biblivre.marc.MarcUtils;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
-import org.marc4j.marc.DataField;
-import org.marc4j.marc.Record;
-import org.marc4j.marc.Subfield;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReportsBO extends AbstractBO {
     private ReportsDAO reportsDAO;
-    private BiblioRecordBO biblioRecordBO;
 
     private Collection<IBiblivreReport> reports;
-    private IndexingGroupBO indexingGroupBO;
 
     public DiskFile generateReport(ReportsDTO dto, TranslationsMap i18n) {
         ReportType type = dto.getType();
@@ -53,113 +35,13 @@ public class ReportsBO extends AbstractBO {
         return this.reportsDAO.searchAuthors(author, database);
     }
 
-    public CustomCountDto getCustomCountData(ReportsDTO reportsDto) {
-        CustomCountDto dto = new CustomCountDto();
-
-        Map<String, Integer> subfieldCounter = new HashMap<>();
-
-        int page = 0;
-        int limit = 100;
-        int offset = limit * page;
-
-        String marcField = reportsDto.getMarcField();
-        String field = marcField.split("_")[0];
-        String subfield = marcField.split("_")[1];
-
-        if (reportsDto.getSearchId() != null && reportsDto.getSearchId() != 0) {
-
-            boolean hasMore = true;
-            while (hasMore) {
-                SearchDTO search = biblioRecordBO.getSearch(reportsDto.getSearchId());
-
-                search.setSort(indexingGroupBO.getDefaultSortableGroupId(RecordType.BIBLIO));
-                search.setIndexingGroup(0);
-                search.getPaging().setRecordsPerPage(limit);
-                search.getPaging().setPage(++page);
-                biblioRecordBO.paginateSearch(search);
-
-                if (search == null || search.size() == 0) {
-                    hasMore = false;
-                } else {
-                    countSubfieldValue(search, subfieldCounter, field, subfield);
-                }
-            }
-
-        } else {
-            RecordDatabase database = reportsDto.getDatabase();
-            if (database == null) {
-                database = RecordDatabase.MAIN;
-            }
-
-            boolean hasMore = true;
-            while (hasMore) {
-                List<RecordDTO> records = biblioRecordBO.list(offset, limit, database);
-                if (records == null || records.size() == 0) {
-                    hasMore = false;
-                } else {
-                    offset = limit * ++page;
-                    countSubfieldValue(records, subfieldCounter, field, subfield);
-                }
-            }
-        }
-
-        List<String[]> data = new ArrayList<String[]>();
-
-        for (Entry<String, Integer> entry : subfieldCounter.entrySet()) {
-            String[] valuePair = new String[2];
-            valuePair[0] = entry.getKey();
-            valuePair[1] = String.valueOf(entry.getValue());
-            data.add(valuePair);
-        }
-
-        dto.setData(data);
-
-        return dto;
-    }
-
-    private void countSubfieldValue(
-            Collection<RecordDTO> records,
-            Map<String, Integer> subfieldCounter,
-            String field,
-            String subfield) {
-
-        for (RecordDTO biblio : records) {
-            Record record = MarcUtils.iso2709ToRecord(biblio.getIso2709());
-            MarcDataReader reader = new MarcDataReader(record);
-            List<DataField> datafields = reader.getDataFields(field);
-
-            for (DataField df : datafields) {
-                List<Subfield> subfields = df.getSubfields(subfield.charAt(0));
-
-                for (Subfield sf : subfields) {
-                    String value = sf.getData();
-                    Integer count = subfieldCounter.get(value);
-                    if (count == null) {
-                        count = 0;
-                    }
-                    subfieldCounter.put(value, ++count);
-                }
-            }
-        }
-    }
-
     @Autowired
     public void setReportsDAO(ReportsDAO reportsDAO) {
         this.reportsDAO = reportsDAO;
     }
 
     @Autowired
-    public void setBiblioRecordBO(BiblioRecordBO biblioRecordBO) {
-        this.biblioRecordBO = biblioRecordBO;
-    }
-
-    @Autowired
     public void setReports(Collection<IBiblivreReport> reports) {
         this.reports = reports;
-    }
-
-    @Autowired
-    public void setIndexingGroupBO(IndexingGroupBO indexingGroupBO) {
-        this.indexingGroupBO = indexingGroupBO;
     }
 }
