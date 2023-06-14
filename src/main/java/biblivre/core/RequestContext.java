3606ea46146f3ba9ce71c5725ff0df6b15e3638a
Cleydyr de Albuquerque <cleydyr@gmail.com>
2023-06-14 17:46:48 +0200

    refactor: use record class

--- src/main/java/biblivre/core/RequestContext.java
+++ src/main/java/biblivre/core/RequestContext.java
@@ -1,36 +1,5 @@
 package biblivre.core;
 
-public class RequestContext {
-    private final ExtendedRequest xRequest;
-    private final ExtendedResponse xResponse;
-    private final boolean headerOnly;
-    private final AbstractHandler handler;
-
-    public RequestContext(
-            ExtendedRequest xRequest,
-            ExtendedResponse xResponse,
-            boolean headerOnly,
-            AbstractHandler handler) {
-        super();
-        this.xRequest = xRequest;
-        this.xResponse = xResponse;
-        this.headerOnly = headerOnly;
-        this.handler = handler;
-    }
-
-    public ExtendedRequest getxRequest() {
-        return xRequest;
-    }
-
-    public ExtendedResponse getxResponse() {
-        return xResponse;
-    }
-
-    public boolean isHeaderOnly() {
-        return headerOnly;
-    }
-
-    public AbstractHandler getHandler() {
-        return handler;
-    }
+public record RequestContext(ExtendedRequest xRequest, ExtendedResponse xResponse, boolean headerOnly,
+                             AbstractHandler handler) {
 }
