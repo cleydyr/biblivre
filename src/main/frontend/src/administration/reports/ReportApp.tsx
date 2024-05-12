30a7e0fcfc6f3540cfb8ca52d85d4a8ab46e9125
Cleydyr de Albuquerque <cleydyr@gmail.com>
2024-05-12 14:56:14 +0200

    refactor

--- src/main/frontend/src/administration/reports/ReportApp.tsx
+++ src/main/frontend/src/administration/reports/ReportApp.tsx
@@ -16,7 +16,7 @@ import {
 } from "@elastic/eui";
 import { Toast } from "@elastic/eui/src/components/toast/global_toast_list";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import {
   Configuration,
   InitOverrideFunction,
@@ -44,96 +44,67 @@ const DEFAULT_FETCH_OPTIONS: InitOverrideFunction = async ({
   },
 });
 
+const toasts: Toast[] = [
+  {
+    id: "report-deleted",
+    title: "Modelo de relatório excluído",
+    color: "success",
+    iconType: "check",
+  },
+  {
+    id: "report-updated",
+    title: "Modelo de relatório atualizado",
+    color: "success",
+    iconType: "check",
+  },
+] as const;
+
+const toastsById = toasts.reduce((acc, toast) => {
+  acc[toast.id] = toast;
+  return acc;
+}, {} as Record<(typeof toasts)[number]["id"], Toast>);
+
 export default function ReportApp() {
   const [screen, setScreen] = useState("list" as Screen);
 
   const [toasts, setToasts] = useState([] as Toast[]);
 
-  const [submitingReport, setSubmitingReport] = useState(
-    undefined as Report | undefined
-  );
-
   function removeToast(toast: Toast): void {
     setToasts(toasts.filter((t) => t.id !== toast.id));
   }
 
+  const handleUpdateReportSuccess = useCallback(() => {
+    setScreen("list");
+    setToasts([toastsById["report-updated"]]);
+  }, []);
+
+  const handleDeleteReportSuccess = useCallback(() => {
+    queryClient.invalidateQueries({ queryKey: [REPORT_QUERY] });
+    setDeletingReport(undefined);
+    setToasts([toastsById["report-deleted"]]);
+  }, []);
+
+  const handleDeleteReportFailure = useCallback((error: Error) => {
+    setErrors((errors) => [...errors, error.message]);
+    setDeletingReport(undefined);
+  }, []);
+
   const queryClient = useQueryClient();
 
-  const apiConfiguration = new Configuration({
-    basePath: "http://localhost:8090/api/v2",
+  const getReportsQuery = useListReportsQuery();
+
+  const { mutate: updateReport } = useUpdateReportMutation({
+    onSuccess: handleUpdateReportSuccess,
   });
 
-  const api = new ReportTemplateApi(apiConfiguration);
-
-  const getReportsQuery = useQuery({
-    queryKey: [REPORT_QUERY],
-    queryFn: () => api.getReportTemplates(DEFAULT_FETCH_OPTIONS),
-  });
-
-  const { mutate: updateReport } = useMutation({
-    mutationKey: [REPORT_QUERY],
-    mutationFn: (reportTemplate: ReportTemplate) =>
-      api.updateReport(
-        {
-          reportTemplateId: reportTemplate.id ?? 0,
-          reportTemplate,
-        },
-        DEFAULT_FETCH_OPTIONS
-      ),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [REPORT_QUERY] });
-      setScreen("list");
-      setToasts([
-        {
-          id: "report-updated",
-          title: "Modelo de relatório atualizado",
-          color: "success",
-          iconType: "check",
-        },
-      ]);
-    },
-  });
-
-  const { mutate: deleteReport } = useMutation({
-    mutationKey: ["delete"],
-    mutationFn: (report: ReportTemplate) =>
-      api.deleteReport(
-        {
-          reportTemplateId: report.id,
-        },
-        DEFAULT_FETCH_OPTIONS
-      ),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: [REPORT_QUERY] });
-      setDeletingReport(undefined);
-      setToasts([
-        {
-          id: "report-deleted",
-          title: "Modelo de relatório excluído",
-          color: "success",
-          iconType: "check",
-        },
-      ]);
-    },
-    onError: (error: Error) => {
-      setErrors((errors) => [...errors, error.message]);
-      setDeletingReport(undefined);
-    },
+  const { mutate: deleteReport } = useDeleteReportMutation({
+    onSuccess: handleDeleteReportSuccess,
+    onError: handleDeleteReportFailure,
   });
 
   const { data: reports, isFetching } = getReportsQuery;
 
-  const { mutate: addReport } = useMutation({
-    mutationKey: ["upload"],
-    mutationFn: ({ title, description, file }: UploadReportFormData) =>
-      api.compileReportTemplate(
-        {
-          name: title,
-          description,
-          file,
-        },
-        DEFAULT_FETCH_OPTIONS
-      ),
+  const { mutate: addReport } = useAddReportMutation({
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [REPORT_QUERY] });
       setScreen("list");
@@ -370,4 +341,91 @@ const EditReportForm = ({ report, onSubmit }: ReportFormProps) => {
       </EuiButton>
     </EuiForm>
   );
+};
+
+const useUpdateReportMutation = (
+  options: Parameters<
+    typeof useMutation<ReportTemplate, Error, ReportTemplate, unknown>
+  >[0]
+) => {
+  const apiConfiguration = new Configuration({
+    basePath: "http://localhost:8090/api/v2",
+  });
+
+  const api = new ReportTemplateApi(apiConfiguration);
+
+  return useMutation({
+    mutationKey: [REPORT_QUERY],
+    mutationFn: (reportTemplate: ReportTemplate) =>
+      api.updateReport(
+        {
+          reportTemplateId: reportTemplate.id ?? 0,
+          reportTemplate,
+        },
+        DEFAULT_FETCH_OPTIONS
+      ),
+    ...options,
+  });
+};
+
+const useDeleteReportMutation = (
+  options: Parameters<
+    typeof useMutation<void, Error, ReportTemplate, unknown>
+  >[0]
+) => {
+  const apiConfiguration = new Configuration({
+    basePath: "http://localhost:8090/api/v2",
+  });
+
+  const api = new ReportTemplateApi(apiConfiguration);
+
+  return useMutation({
+    mutationKey: ["delete"],
+    mutationFn: (report: ReportTemplate) =>
+      api.deleteReport(
+        {
+          reportTemplateId: report.id,
+        },
+        DEFAULT_FETCH_OPTIONS
+      ),
+    ...options,
+  });
+};
+
+const useListReportsQuery = () => {
+  const apiConfiguration = new Configuration({
+    basePath: "http://localhost:8090/api/v2",
+  });
+
+  const api = new ReportTemplateApi(apiConfiguration);
+
+  return useQuery({
+    queryKey: [REPORT_QUERY],
+    queryFn: () => api.getReportTemplates(DEFAULT_FETCH_OPTIONS),
+  });
+};
+
+const useAddReportMutation = (
+  options: Parameters<
+    typeof useMutation<void, Error, ReportTemplate, unknown>
+  >[0]
+) => {
+  const apiConfiguration = new Configuration({
+    basePath: "http://localhost:8090/api/v2",
+  });
+
+  const api = new ReportTemplateApi(apiConfiguration);
+
+  return useMutation({
+    mutationKey: ["upload"],
+    mutationFn: ({ title, description, file }: UploadReportFormData) =>
+      api.compileReportTemplate(
+        {
+          name: title,
+          description,
+          file,
+        },
+        DEFAULT_FETCH_OPTIONS
+      ),
+  });
 };
