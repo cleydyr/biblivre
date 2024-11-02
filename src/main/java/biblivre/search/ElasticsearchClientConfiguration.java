088c8e8d51f4c71cb6be0c7768a6540f026fd8aa
Cleydyr de Albuquerque <cleydyr@gmail.com>
2024-11-02 23:04:19 +0100

    fix: activate only with elasticsearch profile

--- src/main/java/biblivre/search/ElasticsearchClientConfiguration.java
+++ src/main/java/biblivre/search/ElasticsearchClientConfiguration.java
@@ -2,11 +2,13 @@ package biblivre.search;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.support.HttpHeaders;
 
 @Configuration
+@Profile("elasticsearch")
 public class ElasticsearchClientConfiguration extends ElasticsearchConfiguration {
     @Value("${elasticsearch.server.url:}")
     private String serverUrl;
