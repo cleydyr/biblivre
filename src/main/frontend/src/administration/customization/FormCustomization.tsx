f69c5e8594295f3baf43d49b1a84cf7b7fb60cdb
Cleydyr de Albuquerque <cleydyr@gmail.com>
2024-11-04 23:29:56 +0100

    wip: with drag-n-drop

--- src/main/frontend/src/administration/customization/FormCustomization.tsx
+++ src/main/frontend/src/administration/customization/FormCustomization.tsx
@@ -1,33 +1,96 @@
-import {EuiTitle, EuiPanel, EuiLoadingSpinner} from "@elastic/eui";
-import React from "react";
+import {
+    EuiTitle,
+    EuiPanel,
+    EuiLoadingSpinner,
+    EuiDragDropContext,
+    EuiDroppable,
+    EuiDraggable,
+    EuiIcon,
+    EuiFlexGroup,
+    EuiFlexItem,
+    euiDragDropReorder
+} from "@elastic/eui";
+import React, {FC, useEffect, useState} from "react";
 
 import "@elastic/eui/dist/eui_theme_light.css";
-import { RecordType} from "../../generated-sources";
+import {RecordType} from "../../generated-sources";
 import {useFormDataQuery, useTranslationQuery} from "./queries";
+import {OnDragEndResponder} from "@hello-pangea/dnd";
+import type {FormData} from "../../generated-sources";
 
 const App: React.FC = () => {
     const {
-        isLoading: isFormDataLoading,
-        isSuccess: isFormDataSuccess,
+        isLoading,
+        isSuccess,
         data,
     } = useFormDataQuery(RecordType.Biblio);
 
-    const {
-        data: translations,
-        isLoading: isTranslationsLoading,
-        isSuccess: isTranslationsSuccess,
-    } = useTranslationQuery('pt-BR')
-
-    if (isFormDataLoading || isTranslationsLoading) {
+    if (isLoading) {
         return <EuiLoadingSpinner/>
     }
 
-    return isTranslationsSuccess && isFormDataSuccess &&
-        data.map(({datafield}) => (
-            <EuiPanel key={datafield} hasBorder={true}>
-                <EuiTitle size='xs'><h2>{datafield} - {translations?.[`marc.bibliographic.datafield.${datafield}`]}</h2></EuiTitle>
-            </EuiPanel>
-        ));
+    return isSuccess &&
+        <DataFieldsDragAndDrop datafields={data} />
 };
+
+type DataFieldsDragAndDropProps = {
+    datafields: FormData[]
+}
+
+const DataFieldsDragAndDrop: FC<DataFieldsDragAndDropProps> = ({datafields}) => {
+    const {
+        data: translations,
+        isLoading,
+        isSuccess,
+    } = useTranslationQuery('pt-BR')
+
+    const [items, setItems] = useState(datafields)
+
+    if (isLoading) {
+        return <EuiLoadingSpinner/>
+    }
+
+    const onDragEnd: OnDragEndResponder = ({source, destination}) => {
+        if (source && destination) {
+            setItems(euiDragDropReorder(items, source.index, destination.index));
+        }
+    };
+
+    return isSuccess && (<EuiDragDropContext onDragEnd={onDragEnd}>
+        <EuiDroppable
+            droppableId="CUSTOM_HANDLE_DROPPABLE_AREA"
+            spacing="m"
+            withPanel
+        >
+            {
+                items.map(({datafield}, idx) => (
+                    <EuiDraggable
+                        spacing="m"
+                        key={datafield}
+                        index={idx}
+                        draggableId={datafield}
+                        customDragHandle={true}
+                        hasInteractiveChildren={true}
+                    >
+                        {({dragHandleProps}) => (
+                            <EuiPanel hasBorder={true} {...dragHandleProps} >
+                                <EuiFlexGroup gutterSize='m'>
+                                    <EuiFlexItem grow={false}>
+                                        <EuiFlexGroup alignItems="center">
+                                            <EuiIcon type="grab"/>
+                                        </EuiFlexGroup>
+                                    </EuiFlexItem>
+                                    <EuiTitle size='xs'>
+                                        <h2>{datafield} - {translations?.[`marc.bibliographic.datafield.${datafield}`]}</h2>
+                                    </EuiTitle>
+                                </EuiFlexGroup>
+                            </EuiPanel>
+                        )}
+                    </EuiDraggable>
+                ))
+            }
+        </EuiDroppable>
+    </EuiDragDropContext>)
+}
 
 export default App;
