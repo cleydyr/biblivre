97e18df205d588f0cba301c1a21ebeb185921fc6
Cleydyr de Albuquerque <cleydyr@gmail.com>
2025-07-21 01:07:59 +0200

    fixup! feat: add service to create new user custom fields

--- src/main/java/biblivre/circulation/user/UserFieldService.java
+++ src/main/java/biblivre/circulation/user/UserFieldService.java
@@ -19,9 +19,7 @@
  ******************************************************************************/
 package biblivre.circulation.user;
 
-import biblivre.core.SchemaThreadLocal;
 import biblivre.core.translations.TranslationBO;
-import biblivre.core.utils.Constants;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,78 +30,86 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UserFieldService {
 
-  private static final Logger logger = LoggerFactory.getLogger(UserFieldService.class);
+    private static final Logger logger = LoggerFactory.getLogger(UserFieldService.class);
 
-  private final UserFieldsDAO userFieldsDAO;
-  private final TranslationBO translationBO;
+    private final UserFieldsDAO userFieldsDAO;
+    private final TranslationBO translationBO;
 
-  @Autowired
-  public UserFieldService(UserFieldsDAO userFieldsDAO, TranslationBO translationBO) {
-    this.userFieldsDAO = userFieldsDAO;
-    this.translationBO = translationBO;
-  }
-
-  /**
-   * Creates a new user field with translations.
-   * 
-   * @param userField             The user field to persist
-   * @param fieldNameTranslations Map of language codes to translated field names
-   * @return The persisted UserFieldDTO
-   */
-  @Transactional
-  public UserFieldDTO createUserFieldWithTranslations(
-      UserFieldDTO userField, Map<String, String> fieldNameTranslations) {
-
-    logger.info("Creating user field '{}' with translations for {} languages",
-        userField.getKey(), fieldNameTranslations.size());
-
-    // 1. Persist the field in the users_fields table
-    UserFieldDTO persistedField = userFieldsDAO.persistField(userField);
-
-    // 2. Persist the field name translations in the global schema
-    persistFieldNameTranslations(userField.getKey(), fieldNameTranslations);
-
-    logger.info("Successfully created user field '{}' with translations", userField.getKey());
-
-    return persistedField;
-  }
-
-  /**
-   * Persists field name translations in the global schema.
-   * 
-   * @param fieldKey     The field key to create translation keys for
-   * @param translations Map of language codes to translated field names
-   */
-  private void persistFieldNameTranslations(String fieldKey, Map<String, String> translations) {
-    // Generate the translation key for this field
-    String translationKey = "circulation.custom.user_field." + fieldKey;
-
-    // Save translations in the global schema
-    for (Map.Entry<String, String> entry : translations.entrySet()) {
-      String language = entry.getKey();
-      String translatedName = entry.getValue();
-
-      logger.debug("Adding translation for field '{}' in language '{}': '{}'",
-          fieldKey, language, translatedName);
-
-      translationBO.addSingleTranslation(language, translationKey, translatedName);
+    @Autowired
+    public UserFieldService(UserFieldsDAO userFieldsDAO, TranslationBO translationBO) {
+        this.userFieldsDAO = userFieldsDAO;
+        this.translationBO = translationBO;
     }
-    return null;
-  }
 
-  /**
-   * Updates translations for an existing user field.
-   * 
-   * @param fieldKey              The field key
-   * @param fieldNameTranslations Map of language codes to translated field names
-   */
-  @Transactional
-  public void updateFieldTranslations(String fieldKey, Map<String, String> fieldNameTranslations) {
-    logger.info("Updating translations for user field '{}' with {} languages",
-        fieldKey, fieldNameTranslations.size());
+    /**
+     * Creates a new user field with translations.
+     *
+     * @param userField The user field to persist
+     * @param fieldNameTranslations Map of language codes to translated field names
+     * @return The persisted UserFieldDTO
+     */
+    @Transactional
+    public UserFieldDTO createUserFieldWithTranslations(
+            UserFieldDTO userField, Map<String, String> fieldNameTranslations) {
 
-    persistFieldNameTranslations(fieldKey, fieldNameTranslations);
+        logger.info(
+                "Creating user field '{}' with translations for {} languages",
+                userField.getKey(),
+                fieldNameTranslations.size());
 
-    logger.info("Successfully updated translations for user field '{}'", fieldKey);
-  }
+        // 1. Persist the field in the users_fields table
+        UserFieldDTO persistedField = userFieldsDAO.persistField(userField);
+
+        // 2. Persist the field name translations in the global schema
+        persistFieldNameTranslations(userField.getKey(), fieldNameTranslations);
+
+        logger.info("Successfully created user field '{}' with translations", userField.getKey());
+
+        return persistedField;
+    }
+
+    /**
+     * Persists field name translations in the global schema.
+     *
+     * @param fieldKey The field key to create translation keys for
+     * @param translations Map of language codes to translated field names
+     */
+    private void persistFieldNameTranslations(String fieldKey, Map<String, String> translations) {
+        // Generate the translation key for this field
+        String translationKey = "circulation.custom.user_field." + fieldKey;
+
+        // Save translations in the global schema
+        for (Map.Entry<String, String> entry : translations.entrySet()) {
+            String language = entry.getKey();
+            String translatedName = entry.getValue();
+
+            logger.debug(
+                    "Adding translation for field '{}' in language '{}': '{}'",
+                    fieldKey,
+                    language,
+                    translatedName);
+
+            translationBO.addSingleTranslation(language, translationKey, translatedName);
+        }
+        return null;
+    }
+
+    /**
+     * Updates translations for an existing user field.
+     *
+     * @param fieldKey The field key
+     * @param fieldNameTranslations Map of language codes to translated field names
+     */
+    @Transactional
+    public void updateFieldTranslations(
+            String fieldKey, Map<String, String> fieldNameTranslations) {
+        logger.info(
+                "Updating translations for user field '{}' with {} languages",
+                fieldKey,
+                fieldNameTranslations.size());
+
+        persistFieldNameTranslations(fieldKey, fieldNameTranslations);
+
+        logger.info("Successfully updated translations for user field '{}'", fieldKey);
+    }
 }
