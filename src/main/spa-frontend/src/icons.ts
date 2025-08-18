8baa9351f0cdcfe111623c0d249959bf08ab7a4e
Cleydyr de Albuquerque <cleydyr@gmail.com>
2025-08-18 09:21:45 +0200

    style: remove semicolons

--- src/main/spa-frontend/src/icons.ts
+++ src/main/spa-frontend/src/icons.ts
@@ -1,17 +1,18 @@
-import type { ICON_TYPES } from '@elastic/eui';
-import { icon as logoElastic } from '@elastic/eui/es/components/icon/assets/logo_elastic';
-import { icon as logoElasticsearch } from '@elastic/eui/es/components/icon/assets/logo_elasticsearch';
-import { icon as stopFilled } from '@elastic/eui/es/components/icon/assets/stop_filled';
-import { appendIconComponentCache } from '@elastic/eui/es/components/icon/icon';
-import type { ValuesType } from 'utility-types';
+import { icon as logoElastic } from '@elastic/eui/es/components/icon/assets/logo_elastic'
+import { icon as logoElasticsearch } from '@elastic/eui/es/components/icon/assets/logo_elasticsearch'
+import { icon as stopFilled } from '@elastic/eui/es/components/icon/assets/stop_filled'
+import { appendIconComponentCache } from '@elastic/eui/es/components/icon/icon'
 
-type IconComponentNameType = ValuesType<typeof ICON_TYPES>;
-type IconComponentCacheType = Partial<Record<IconComponentNameType, unknown>>;
+import type { ICON_TYPES } from '@elastic/eui'
+import type { ValuesType } from 'utility-types'
+
+type IconComponentNameType = ValuesType<typeof ICON_TYPES>
+type IconComponentCacheType = Partial<Record<IconComponentNameType, unknown>>
 
 const cachedIcons: IconComponentCacheType = {
   logoElasticsearch,
   logoElastic,
   stopFilled,
-};
+}
 
-appendIconComponentCache(cachedIcons);
+appendIconComponentCache(cachedIcons)
