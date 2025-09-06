2003c26741ff65dd99b7ea5624dbb37545c585a8
Cleydyr de Albuquerque <cleydyr@gmail.com>
2025-09-06 22:43:54 +0200

    feat: add advanced search switch

--- src/main/spa-frontend/src/search/BibliographicSearchPage.tsx
+++ src/main/spa-frontend/src/search/BibliographicSearchPage.tsx
@@ -1,10 +1,11 @@
-import { EuiButton } from '@elastic/eui'
+import { EuiButton, EuiFlexGroup, EuiSwitch } from '@elastic/eui'
 import { Fragment, useState } from 'react'
-
 import BibliographicSearchResultsTable from './BibliographicSearchResultsTable'
 import { useSearchCatalographic } from './hooks'
+import { useToggle } from '@uidotdev/usehooks'
 
 import type { BibliographicRecord } from '../api-helpers/search/response-types'
+import { FormattedMessage } from 'react-intl'
 
 const ConnectedBibliographicSearchPage = () => {
   const {
@@ -17,20 +18,37 @@ const ConnectedBibliographicSearchPage = () => {
 
   const [_, setSelectedRecords] = useState<BibliographicRecord[]>([])
 
+  const [isAdvancedSearch, toggleAdvancedSearch] = useToggle(false)
+
   if (isError) {
     return <div>Error</div>
   }
 
   return (
     <Fragment>
-      <EuiButton
-        isLoading={isPending}
-        onClick={() => {
-          search()
-        }}
-      >
-        Search
-      </EuiButton>
+      <EuiFlexGroup>
+        <EuiButton
+          isLoading={isPending}
+          onClick={() => {
+            search()
+          }}
+        >
+          <FormattedMessage
+            id='search.bibliographic.search_all'
+            defaultMessage='Listar todos'
+          />
+        </EuiButton>
+        <EuiSwitch
+          checked={isAdvancedSearch}
+          onChange={() => toggleAdvancedSearch()}
+          label={
+            <FormattedMessage
+              id='search.bibliographic.advanced_search'
+              defaultMessage='Pesquisa avançada'
+            />
+          }
+        />
+      </EuiFlexGroup>
       {isSuccess && (
         <BibliographicSearchResultsTable
           isPending={isPending}
