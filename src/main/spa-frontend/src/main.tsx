e045d93a2dfb30831c23e3d4622ee742c8904ee2
Cleydyr de Albuquerque <cleydyr@gmail.com>
2025-09-07 00:48:36 +0200

    feat: add browser router provider to the app

--- src/main/spa-frontend/src/main.tsx
+++ src/main/spa-frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { IntlProvider } from 'react-intl'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
 
@@ -13,12 +14,14 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <IntlProvider locale='pt-BR'>
-      <EuiProvider>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </EuiProvider>
-    </IntlProvider>
+    <BrowserRouter>
+      <IntlProvider locale='pt-BR'>
+        <EuiProvider>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </EuiProvider>
+      </IntlProvider>
+    </BrowserRouter>
   </StrictMode>
 )
