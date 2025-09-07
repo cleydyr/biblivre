af4f303578d9abf37679cbf50861ae46806b646c
Cleydyr de Albuquerque <cleydyr@gmail.com>
2025-09-07 21:04:37 +0200

    feat: add i18n to eui components

---
+++ src/main/spa-frontend/src/i18n.ts
@@ -0,0 +1,7 @@
+export const i18n = {
+  locale: 'pt-BR',
+  mapping: {
+    'euiPaginationButtonArrow.nextPage': 'Próxima página',
+    'euiPaginationButtonArrow.previousPage': 'Página anterior',
+  },
+}
--- src/main/spa-frontend/src/main.tsx
+++ src/main/spa-frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import './icons.ts'
 
-import { EuiProvider } from '@elastic/eui'
+import { EuiContext, EuiProvider } from '@elastic/eui'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
@@ -8,6 +8,7 @@ import { IntlProvider } from 'react-intl'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App.tsx'
+import { i18n } from './i18n.ts'
 
 // Create a client
 const queryClient = new QueryClient()
@@ -17,9 +18,11 @@ createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
       <IntlProvider locale='pt-BR'>
         <EuiProvider>
-          <QueryClientProvider client={queryClient}>
-            <App />
-          </QueryClientProvider>
+          <EuiContext i18n={i18n}>
+            <QueryClientProvider client={queryClient}>
+              <App />
+            </QueryClientProvider>
+          </EuiContext>
         </EuiProvider>
       </IntlProvider>
     </BrowserRouter>
