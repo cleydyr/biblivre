894dccd798990a0b62683cfbb27c7129cb5ffa8c
Cleydyr de Albuquerque <cleydyr@gmail.com>
2025-09-07 23:41:39 +0200

    build: replace vanilla react plugin with swc

--- src/main/spa-frontend/vite.config.ts
+++ src/main/spa-frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
 })
