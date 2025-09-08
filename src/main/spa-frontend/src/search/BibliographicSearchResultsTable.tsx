fef93220e4927ac998f388c2ae4c4f7d4c377e33
Cleydyr de Albuquerque <cleydyr@gmail.com>
2025-09-08 21:53:12 +0200

    feat: use automatic layout for result table

--- src/main/spa-frontend/src/search/BibliographicSearchResultsTable.tsx
+++ src/main/spa-frontend/src/search/BibliographicSearchResultsTable.tsx
@@ -44,6 +44,7 @@ export const BibliographicSearchResultsTable: FC<Props> = ({
       }
       pagination={pagination}
       selection={selection}
+      tableLayout='auto'
       onChange={onChange}
     />
   )
