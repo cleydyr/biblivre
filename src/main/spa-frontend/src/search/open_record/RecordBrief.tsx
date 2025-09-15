3508acb69c005f0f2efbf3ab0779c6ad35db23a8
Cleydyr de Albuquerque <cleydyr@gmail.com>
2025-09-16 01:50:19 +0200

    WIP rename

---
+++ src/main/spa-frontend/src/search/open_record/RecordBrief.tsx
@@ -0,0 +1,34 @@
+import { EuiDescriptionList, EuiFlexGroup } from '@elastic/eui'
+
+import { getLegacyMarcDatafieldTranslation } from '../../legacy_translations/lib'
+
+import type { FC } from 'react'
+
+import type { OpenResult } from '../../api-helpers/search/response-types'
+
+type Props = {
+  record: OpenResult
+}
+
+const RecordBrief: FC<Props> = ({ record }) => {
+  return (
+    <EuiFlexGroup direction='column'>
+      <div />
+      <EuiDescriptionList
+        compressed
+        listItems={usePanelDescriptionListItems(record)}
+        type='column'
+      />
+    </EuiFlexGroup>
+  )
+}
+
+function usePanelDescriptionListItems(record: OpenResult) {
+  return record.fields.map((field) => {
+    return {
+      title: getLegacyMarcDatafieldTranslation(field.datafield),
+      description: field.value,
+    }
+  })
+}
+export default RecordBrief
