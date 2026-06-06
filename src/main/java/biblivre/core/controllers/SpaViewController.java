d0391f3d27ab5cbdb7a61cab8a6e6f9f31a707ba
Cleydyr Bezerra de Albuquerque <cleydyr@gmail.com>
2026-06-06 22:29:38 +0200

    fix: after upgrade to spring boot 4

--- src/main/java/biblivre/core/controllers/SpaViewController.java
+++ src/main/java/biblivre/core/controllers/SpaViewController.java
@@ -1,8 +1,7 @@
 package biblivre.core.controllers;
 
 import biblivre.core.utils.Constants;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,19 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SpaViewController {
 
-    private final ObjectMapper objectMapper;
-
-    public SpaViewController(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
     @GetMapping("/spa/**")
-    public String spaPage(Model model) throws JsonProcessingException {
+    public String spaPage(Model model) {
         String key = System.getenv(Constants.FLAGSMITH_ENVIRONMENT_KEY);
         if (key == null) {
             key = "";
         }
-        model.addAttribute("flagsmithEnvironmentKeyJson", objectMapper.writeValueAsString(key));
+        model.addAttribute("flagsmithEnvironmentKeyJson", JSONObject.quote(key));
         return "spa";
     }
 }
